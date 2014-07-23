33c087e660efb109789032658be833f18e69bddd
Valentine Rech de Laval <valentine.rechdelaval@isb-sib.ch>
2014-07-23 16:57:40 +0000

    - Minor edits to be HTML5 and W3C compliant. - Add <img> in <a> for logo to improve accessibility. - Fix SIB footer that wasn't at the bottom when height page was over 1300px.

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
@@ -12,221 +12,226 @@ import org.bgee.view.DisplayParentInterface;
 
 public class HtmlParentDisplay extends ConcreteDisplayParent implements DisplayParentInterface
 {
-	private int uniqueId;
-	protected RequestParameters requestParameters;
-	protected final static String parametersSeparator = "&amp;";
+    private int uniqueId;
+    protected RequestParameters requestParameters;
+    protected final static String parametersSeparator = "&amp;";
 
-	public HtmlParentDisplay(HttpServletResponse response, RequestParameters requestParameters) throws IOException
-	{
-		super(response);
-		this.uniqueId = 0;
-		this.requestParameters = requestParameters;
-	}
+    public HtmlParentDisplay(HttpServletResponse response, RequestParameters requestParameters) throws IOException
+    {
+        super(response);
+        this.uniqueId = 0;
+        this.requestParameters = requestParameters;
+    }
 
-	protected int getUniqueId()
-	{
-		//need to return 0 the first time this method is called;
-		int idToReturn = this.uniqueId;
-		this.uniqueId++;
-		return idToReturn;
-	}
+    protected int getUniqueId()
+    {
+        //need to return 0 the first time this method is called;
+        int idToReturn = this.uniqueId;
+        this.uniqueId++;
+        return idToReturn;
+    }
 
-	@Override
-	public void emptyDisplay()
-	{
-		this.sendHeaders(true);
-		this.writeln("");
-	}
-	@Override
-	public void startDisplay(String page, String title)
-	{
-		this.writeln("<!DOCTYPE html>");
-		this.writeln("<html lang='en'>");
-		this.writeln("<head>");
-		this.writeln("<meta http-equiv='content-type' content='text/html; utf-8'>");
-		this.writeln("<title>"+title+"</title>");
-		this.writeln("<meta name='description' content='Bgee allows to automatically"
-				+ " compare gene expression patterns between species, by referencing"
-				+ " expression data on anatomical ontologies, and designing homology"
-				+ " relationships between them.'/>");
-		this.writeln("<meta name='keywords' content='bgee, gene expression, "
-				+ "evolution, ontology, anatomy, development, evo-devo database, "
-				+ "anatomical ontology, developmental ontology, gene expression "
-				+ "evolution'/>");
-		this.writeln("<meta name='dcterms.rights' content='Bgee copyright 2007/2014 UNIL' />");
-		this.writeln("<link rel='shortcut icon' type='image/x-icon' href='"
-				+BgeeProperties.getImagesRootDirectory()+"favicon.ico'/>");
-		this.writeln("<link rel='stylesheet' type='text/css' href='"
-				+BgeeProperties.getCssFilesRootDirectory()+page+".css' />");
-		this.writeln("<link rel='stylesheet' type='text/css' href='"
-				+BgeeProperties.getCssFilesRootDirectory()+"jquery-ui.css' />");
-		this.includeJs();
-		this.writeln("</head>");
-		this.writeln("<body>");
+    @Override
+    public void emptyDisplay()
+    {
+        this.sendHeaders(true);
+        this.writeln("");
+    }
+    @Override
+    public void startDisplay(String page, String title)
+    {
+        this.writeln("<!DOCTYPE html>");
+        this.writeln("<html lang='en'>");
+        this.writeln("<head>");
+        this.writeln("<meta charset='UTF-8'>");
+        this.writeln("<title>"+title+"</title>");
+        this.writeln("<meta name='description' content='Bgee allows to automatically"
+                + " compare gene expression patterns between species, by referencing"
+                + " expression data on anatomical ontologies, and designing homology"
+                + " relationships between them.'/>");
+        this.writeln("<meta name='keywords' content='bgee, gene expression, "
+                + "evolution, ontology, anatomy, development, evo-devo database, "
+                + "anatomical ontology, developmental ontology, gene expression "
+                + "evolution'/>");
+        this.writeln("<meta name='dcterms.rights' content='Bgee copyright 2007/2014 UNIL' />");
+        this.writeln("<link rel='shortcut icon' type='image/x-icon' href='"
+                +BgeeProperties.getImagesRootDirectory()+"favicon.ico'/>");
+        this.writeln("<link rel='stylesheet' type='text/css' href='"
+                +BgeeProperties.getCssFilesRootDirectory()+page+".css' />");
+        this.writeln("<link rel='stylesheet' type='text/css' href='"
+                +BgeeProperties.getCssFilesRootDirectory()+"jquery-ui.css' />");
+        this.includeJs();
+        this.writeln("</head>");
+        this.writeln("<body>");
         this.writeln("<noscript>Sorry, your browser does not support JavaScript!</noscript>");
-		this.displayBgeeMenu();
-		this.writeln("<div id='bgee_top'><a id='TOP'></a></div>");
+        this.displayBgeeMenu();
+        this.writeln("<div id='bgee_top'><a id='TOP'></a></div>");
         this.writeln("<div id='sib_container'>");
-	}
+    }
 
-	public void endDisplay()
-	{
-	    this.writeln("<div id = 'sib_footer'>");
-	    this.writeln("<div id = 'sib_footer_content'>");
-	    this.writeln("<a href = 'http://www.isb-sib.ch'>Swiss Institute of Bioinformatics</a>");
-	    this.writeln("<div id = 'sib_footer_right'>");
-	    this.writeln("<a href='#TOP' id = 'sib_footer_gototop'>"
-	            + "<span style = 'padding-left: 10px'>Back to the Top</span></a>");
-	    this.writeln("</div>");
-	    this.writeln("</div>");
-	    this.writeln("</div>");
-	    this.writeln("</div>");
-	    this.writeln("</body>");
-	    this.writeln("</html>");
-	}
+    public void endDisplay()
+    {
+        this.writeln("</div>");
+        this.writeln("<footer>");
+        this.writeln("<div id = 'sib_footer_content'>");
+        this.writeln("<a href = 'http://www.isb-sib.ch'>SIB Swiss Institute of Bioinformatics</a>");
+        this.writeln("<div id = 'sib_footer_right'>");
+        this.writeln("<a href='#TOP' id = 'sib_footer_gototop'>"
+                + "<span style = 'padding-left: 10px'>Back to the Top</span></a>");
+        this.writeln("</div>");
+        this.writeln("</div>");
+        this.writeln("</footer>");
+        this.writeln("</div>");
+        this.writeln("</body>");
+        this.writeln("</html>");
+    }
 
-	// TODO move into BgeeStringUtils ??? 
-	public static String htmlEntities(String stringToWrite)
-	{
-		try {							
-			// TODO check if new version, HTML5
-			return StringEscapeUtils.escapeHtml4(stringToWrite).replaceAll("'", "&apos;");
-		} catch (Exception e) {
-			return "";
-		}
-	}
+    // TODO move into BgeeStringUtils ??? 
+    public static String htmlEntities(String stringToWrite)
+    {
+        try {                            
+            // TODO check if new version, HTML5
+            return StringEscapeUtils.escapeHtml4(stringToWrite).replaceAll("'", "&apos;");
+        } catch (Exception e) {
+            return "";
+        }
+    }
 
-	public String nl2br(String string)
-	{
-		String localString = string;
-		localString = localString.replaceAll("\r\n", "\n");
-		localString = localString.replaceAll("\r",   "\n");	
-		localString = localString.replaceAll("\n",   "<br/>");
-		return localString;
-	}
+    public String nl2br(String string)
+    {
+        String localString = string;
+        localString = localString.replaceAll("\r\n", "\n");
+        localString = localString.replaceAll("\r",   "\n");    
+        localString = localString.replaceAll("\n",   "<br/>");
+        return localString;
+    }
 
-	@Override
-	public void sendHeaders(boolean ajax)
-	{
-		if (this.response == null) {
-			return;
-		}
-		if (!this.headersAlreadySent) {
-			this.response.setContentType("text/html");
-			if (ajax) {
-				this.response.setDateHeader("Expires", 1);
-				this.response.setHeader("Cache-Control", 
-						"no-store, no-cache, must-revalidate, proxy-revalidate");
-				this.response.addHeader("Cache-Control", "post-check=0, pre-check=0");
-				this.response.setHeader("Pragma", "No-cache");
-			}
-			this.headersAlreadySent = true;
-		}
-	}
+    @Override
+    public void sendHeaders(boolean ajax)
+    {
+        if (this.response == null) {
+            return;
+        }
+        if (!this.headersAlreadySent) {
+            this.response.setContentType("text/html");
+            if (ajax) {
+                this.response.setDateHeader("Expires", 1);
+                this.response.setHeader("Cache-Control", 
+                        "no-store, no-cache, must-revalidate, proxy-revalidate");
+                this.response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+                this.response.setHeader("Pragma", "No-cache");
+            }
+            this.headersAlreadySent = true;
+        }
+    }
 
-	public void sendServiceUnavailableHeaders()
-	{
-		if (this.response == null) {
-			return;
-		}
-		if (!this.headersAlreadySent) {
-			this.response.setContentType("text/html");
-			this.response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
-			this.headersAlreadySent = true;
-		}
-	}
+    public void sendServiceUnavailableHeaders()
+    {
+        if (this.response == null) {
+            return;
+        }
+        if (!this.headersAlreadySent) {
+            this.response.setContentType("text/html");
+            this.response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+            this.headersAlreadySent = true;
+        }
+    }
 
-	protected void sendBadRequestHeaders()
-	{
-		if (this.response == null) {
-			return;
-		}
-		if (!this.headersAlreadySent) {
-			this.response.setContentType("text/html");
-			this.response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			this.headersAlreadySent = true;
-		}
-	}
+    protected void sendBadRequestHeaders()
+    {
+        if (this.response == null) {
+            return;
+        }
+        if (!this.headersAlreadySent) {
+            this.response.setContentType("text/html");
+            this.response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            this.headersAlreadySent = true;
+        }
+    }
 
-	protected void sendPageNotFoundHeaders()
-	{
-		if (this.response == null) {
-			return;
-		}
-		if (!this.headersAlreadySent) {
-			this.response.setContentType("text/html");
-			this.response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-			this.headersAlreadySent = true;
-		}
-	}
+    protected void sendPageNotFoundHeaders()
+    {
+        if (this.response == null) {
+            return;
+        }
+        if (!this.headersAlreadySent) {
+            this.response.setContentType("text/html");
+            this.response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            this.headersAlreadySent = true;
+        }
+    }
 
-	protected void sendInternalErrorHeaders()
-	{
-		if (this.response == null) {
-			return;
-		}
-		if (!this.headersAlreadySent) {
-			this.response.setContentType("text/html");
-			this.response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			this.headersAlreadySent = true;
-		}
-	}
+    protected void sendInternalErrorHeaders()
+    {
+        if (this.response == null) {
+            return;
+        }
+        if (!this.headersAlreadySent) {
+            this.response.setContentType("text/html");
+            this.response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            this.headersAlreadySent = true;
+        }
+    }
 
-	public String urlEncode(String string)
-	{
-		if (string == null) {
-			return null;
-		}
-		String encodeString = string;
-		try {
-			// warning, you need to add an attribut to the connector in server.xml  
-			// in order to get the utf-8 encoding working : URIEncoding="UTF-8"
-			encodeString = java.net.URLEncoder.encode(string, "ISO-8859-1");
-		} catch (Exception e) {
+    public String urlEncode(String string)
+    {
+        if (string == null) {
+            return null;
+        }
+        String encodeString = string;
+        try {
+            // warning, you need to add an attribut to the connector in server.xml  
+            // in order to get the utf-8 encoding working : URIEncoding="UTF-8"
+            encodeString = java.net.URLEncoder.encode(string, "ISO-8859-1");
+        } catch (Exception e) {
 
-		}
-		return encodeString;
-	}
+        }
+        return encodeString;
+    }
 
-	protected String displayHelpLink(String cat, String display)
-	{
-		return "<span class='help'><a href='#' class='help|" + 
-				cat + "'>" + display + "</a></span>";
-	}
-	protected String displayHelpLink(String cat)
-	{
-		return this.displayHelpLink(cat, "[?]");
-	}
+    protected String displayHelpLink(String cat, String display)
+    {
+        return "<span class='help'><a href='#' class='help|" + 
+                cat + "'>" + display + "</a></span>";
+    }
+    protected String displayHelpLink(String cat)
+    {
+        return this.displayHelpLink(cat, "[?]");
+    }
 
-	@Override
-	public void displayBgeeMenu() {
-		this.writeln("<div id='sib_header'>");
+    @Override
+    public void displayBgeeMenu() {
+        this.writeln("<header>");
+ 
         // Bgee logo
-		this.writeln("<div>");
-		this.writeln("<a href='#' id='sib_other_logo' "
-		        + "title='Bgee: a dataBase for Gene Expression Evolution'></a>");
-        this.writeln("</div>");
+        this.writeln("<a href='#' title='Go to Bgee home page'>"
+                + "<img id='sib_other_logo' src='"+BgeeProperties.getImagesRootDirectory()+"bgee_logo.png' "
+                       + "title='Bgee: a dataBase for Gene Expression Evolution' "
+                       + "alt='Bgee: a dataBase for Gene Expression Evolution' /> "
+                + "</a>");
+
         // Title
-        this.writeln("<div>");
-		this.writeln("<a href='#'>"
-		        + "<h1>Bgee: Gene Expression Evolution</h1>"
-		        + "<h2>Download release 13</h2></a>");
-        this.writeln("</div>");
+        this.writeln("<h1>Bgee: Gene Expression Evolution</h1>"
+                + "<h2>Release 13 download page</h2>"
+                );
+
         // SIB logo
-        this.writeln("<div>");
-		this.writeln("<a href='http://www.isb-sib.ch/' id='sib_logo' "
-		        + "title='SIB Swiss Institute of Bioinformatics'></a>");
-        this.writeln("</div>");
-        this.writeln("</div>");
-	}
+        this.writeln("<a href='http://www.isb-sib.ch/' target='_blank' title='Link to the Swiss Institute of Bioinformatics' >"
+                + "<img id='sib_logo' src='"+BgeeProperties.getImagesRootDirectory()+"sib_logo_141x75.png' "
+                       + "title='Bgee is part of the Swiss Institute of Bioinformatics' "
+                       + "alt='SIB Swiss Institute of Bioinformatics' /> "
+                + "</a>");
 
-	protected void includeJs(){
+        this.writeln("</header>");
+    }
 
-	}
+    protected void includeJs(){
 
-	protected void includeJs(String filename){
-		this.writeln("<script  type='text/javascript' src='"+
-		        BgeeProperties.getJavascriptFilesRootDirectory()+filename+"'></script>");
-	}
+    }
+
+    protected void includeJs(String filename){
+        this.writeln("<script  type='text/javascript' src='"+
+                BgeeProperties.getJavascriptFilesRootDirectory()+filename+"'></script>");
+    }
 
 }

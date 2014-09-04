d727a98e3007e95e9e62a15b8cce5e183a3958d4
Valentine Rech de Laval <valentine.rechdelaval@isb-sib.ch>
2014-09-04 22:28:13 +0000

    - Implement getAllAnatEntityRelations() and insertAnatEntityRelations(). - Add MySQLRelationTOResultSet.

--- bgee-dao-sql/src/main/java/org/bgee/model/dao/mysql/ontologycommon/MySQLRelationDAO.java
+++ bgee-dao-sql/src/main/java/org/bgee/model/dao/mysql/ontologycommon/MySQLRelationDAO.java
@@ -1,16 +1,21 @@
 package org.bgee.model.dao.mysql.ontologycommon;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Set;
+import java.util.Map.Entry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.model.dao.mysql.MySQLDAO;
 import org.bgee.model.dao.mysql.connector.BgeePreparedStatement;
 import org.bgee.model.dao.mysql.connector.MySQLDAOManager;
+import org.bgee.model.dao.mysql.connector.MySQLDAOResultSet;
 import org.bgee.model.dao.api.exception.DAOException;
 import org.bgee.model.dao.api.ontologycommon.RelationDAO;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTO.RelationStatus;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTO.RelationType;
 
 /**
  * A {@code RelationDAO} for MySQL. 
@@ -41,15 +46,105 @@ public class MySQLRelationDAO extends MySQLDAO<RelationDAO.Attribute>
     @Override
     public RelationTOResultSet getAllAnatEntityRelations(Set<String> speciesIds) {
         log.entry(speciesIds);
-        // TODO Auto-generated method stub
-        return log.exit(null);
+        
+        StringBuilder sql = new StringBuilder(); 
+        Collection<RelationDAO.Attribute> attributes = this.getAttributes();
+        if (attributes == null || attributes.size() == 0) {
+            sql.append("SELECT *");
+        } else {
+            for (RelationDAO.Attribute attribute: attributes) {
+                if (sql.length() == 0) {
+                    sql.append("SELECT ");
+                } else {
+                    sql.append(", ");
+                }
+                sql.append("anatEntityRelation.");
+                sql.append(this.attributeAnatEntityRelationToString(attribute));
+            }
+        }
+        sql.append(" FROM anatEntityRelation");
+         if (speciesIds != null && speciesIds.size() > 0) {
+             sql.append(" INNER JOIN anatEntityRelationTaxonConstraint ON (" +
+                                 "anatEntityRelationTaxonConstraint.anatEntityRelationId = "
+                                 + "anatEntityRelation.anatEntityRelationId)");
+             sql.append(" WHERE anatEntityRelationTaxonConstraint.speciesId IS NULL");
+             sql.append(" OR anatEntityRelationTaxonConstraint.speciesId IN (");
+             sql.append(createStringFromSet(speciesIds, ','));
+             sql.append(")");
+         }
+
+         //we don't use a try-with-resource, because we return a pointer to the results, 
+         //not the actual results, so we should not close this BgeePreparedStatement.
+         BgeePreparedStatement stmt = null;
+         try {
+             stmt = this.getManager().getConnection().prepareStatement(sql.toString());
+             return log.exit(new MySQLRelationTOResultSet(stmt));
+         } catch (SQLException e) {
+             throw log.throwing(new DAOException(e));
+         }
+    }
+
+    /** 
+     * Returns a {@code String} that correspond to the given {@code RelationDAO.Attribute}.
+     * 
+     * @param attribute   A {code RelationDAO.Attribute} that is the attribute to
+     *                    convert in a {@code String}.
+     * @return            A {@code String} that correspond to the given 
+     *                    {@code RelationDAO.Attribute}
+     */
+    private String attributeAnatEntityRelationToString(RelationDAO.Attribute attribute) {
+        log.entry(attribute);
+        
+        String label = null;
+        if (attribute.equals(RelationDAO.Attribute.RELATIONID)) {
+                label = "anatEntityRelationId";
+        } else if (attribute.equals(RelationDAO.Attribute.SOURCEID)) {
+            label = "anatEntitySourceId";
+        } else if (attribute.equals(RelationDAO.Attribute.TARGETID)) {
+            label = "anatEntityTargetId";
+        } else if (attribute.equals(RelationDAO.Attribute.RELATIONTYPE)) {
+            label = "relationType";
+        } else if (attribute.equals(RelationDAO.Attribute.RELATIONSTATUS)) {
+            label = "relationStatus";
+        } else {
+            throw log.throwing(new IllegalStateException("The attribute provided (" +
+                    attribute.toString() + ") is unknown for " + RelationDAO.class.getName()));
+        }
+        
+        return log.exit(label);
     }
 
     @Override
-    public int insertAnatEntityRelations(Collection<RelationTO> relationTOs) {
-        log.entry(relationTOs);
-        // TODO Auto-generated method stub
-        return log.exit(0);
+    public int insertAnatEntityRelations(Collection<RelationTO> relations) {
+        log.entry(relations);
+
+        // And we need to build two different queries. 
+        String sqlExpression = "INSERT INTO anatEntityRelation " +
+                "(anatEntityRelationId, anatEntitySourceId, anatEntityTargetId, " +
+                " relationType, relationStatus) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        
+        // To not overload MySQL with an error com.mysql.jdbc.PacketTooBigException, 
+        // and because of laziness, we insert expression calls one at a time
+        int relationInsertedCount = 0;
+        try (BgeePreparedStatement stmt = 
+                this.getManager().getConnection().prepareStatement(sqlExpression)) {
+            for (RelationTO relation: relations) {
+                stmt.setInt(1, Integer.parseInt(relation.getRelationId()));
+                stmt.setString(2, relation.getSourceId());
+                stmt.setString(3, relation.getTargetId());
+                stmt.setString(4, RelationTO.convertRelationTypeToDatasourceEnum(
+                        relation.getRelationType()));
+                stmt.setString(5, RelationTO.convertRelationStatusToDatasourceEnum(
+                        relation.getRelationStatus()));
+                relationInsertedCount += stmt.executeUpdate();
+                stmt.clearParameters();
+            }
+        } catch (SQLException e) {
+            throw log.throwing(new DAOException(e));
+        }
+
+        return log.exit(relationInsertedCount);
     }
 
     /**
@@ -89,5 +184,55 @@ public class MySQLRelationDAO extends MySQLDAO<RelationDAO.Attribute>
             throw log.throwing(new DAOException(e));
         }
     }
+    
+    /**
+     * A {@code MySQLDAOResultSet} specific to {@code RelationTO}.
+     * 
+     * @author Valentine Rech de Laval
+     * @version Bgee 13
+     * @since Bgee 13
+     */
+    public class MySQLRelationTOResultSet extends MySQLDAOResultSet<RelationTO> 
+                                          implements RelationTOResultSet {
+        /**
+         * Delegates to {@link MySQLDAOResultSet#MySQLDAOResultSet(BgeePreparedStatement)
+         * super constructor.
+         * 
+         * @param statement The first {@code BgeePreparedStatement} to execute a query on.
+         */
+        public MySQLRelationTOResultSet(BgeePreparedStatement statement) {
+            super(statement);
+        }
 
+        @Override
+        public RelationTO getTO() throws DAOException {
+            log.entry();
+            String relationId = null, sourceId = null, targetId =null;
+            RelationType relationType = null;
+            RelationStatus relationStatus = null;
+            
+            ResultSet currentResultSet = this.getCurrentResultSet();
+            for (Entry<Integer, String> column: this.getColumnLabels().entrySet()) {
+                try {
+                    if (column.getValue().equals("anatEntityRelationId")) {
+                        relationId = currentResultSet.getString(column.getKey());
+                    } else if (column.getValue().equals("anatEntitySourceId") || column.getValue().equals("goAllSourceId") ) {
+                        sourceId = currentResultSet.getString(column.getKey());
+                    } else if (column.getValue().equals("anatEntityTargetId") || column.getValue().equals("goAllTargetId") ) {
+                        targetId = currentResultSet.getString(column.getKey());
+                    } else if (column.getValue().equals("relationStatus")) {
+                        relationStatus = RelationTO.convertDatasourceEnumToRelationStatus(
+                                currentResultSet.getString(column.getKey()));
+                    } else if (column.getValue().equals("relationType")) {
+                        relationType = RelationTO.convertDatasourceEnumToRelationType(
+                                currentResultSet.getString(column.getKey()));
+                    }
+                } catch (SQLException e) {
+                    throw log.throwing(new DAOException(e));
+                }
+            }
+            return log.exit(new RelationTO(relationId, sourceId, targetId, 
+                    relationType, relationStatus));
+        }
+    }
 }

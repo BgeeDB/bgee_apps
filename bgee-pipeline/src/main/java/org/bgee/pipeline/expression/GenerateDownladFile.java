543105ec8e377a06f40858a90c062484beb32673
Valentine Rech de Laval <valentine.rechdelaval@isb-sib.ch>
2014-09-10 14:43:04 +0000

    Remove unnecessary imports.

--- bgee-pipeline/src/main/java/org/bgee/pipeline/expression/GenerateDownladFile.java
+++ bgee-pipeline/src/main/java/org/bgee/pipeline/expression/GenerateDownladFile.java
@@ -1,6 +1,5 @@
 package org.bgee.pipeline.expression;
 
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
@@ -12,10 +11,7 @@ import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bgee.model.dao.api.exception.DAOException;
 import org.bgee.pipeline.Utils;
-import org.obolibrary.oboformat.parser.OBOFormatParserException;
-import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.supercsv.cellprocessor.constraint.IsElementOf;
 import org.supercsv.cellprocessor.constraint.NotNull;
 import org.supercsv.cellprocessor.constraint.UniqueHashCode;

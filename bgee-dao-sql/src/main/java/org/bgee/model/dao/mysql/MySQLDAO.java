8d62167836d85ebb19fb85d84e736dc97ae5039b
Valentine Rech de Laval <valentine.rechdelaval@isb-sib.ch>
2014-10-08 10:07:15 +0000

    Remove unnecessary imports.

--- bgee-dao-sql/src/main/java/org/bgee/model/dao/mysql/MySQLDAO.java
+++ bgee-dao-sql/src/main/java/org/bgee/model/dao/mysql/MySQLDAO.java
@@ -1,18 +1,13 @@
 package org.bgee.model.dao.mysql;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.model.dao.api.DAO;
-import org.bgee.model.dao.api.DAOResultSet;
-import org.bgee.model.dao.api.TransferObject;
-import org.bgee.model.dao.api.exception.DAOException;
 import org.bgee.model.dao.mysql.connector.MySQLDAOManager;
 
 /**

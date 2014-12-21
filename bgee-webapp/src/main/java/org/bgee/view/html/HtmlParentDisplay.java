de9ebd32668a1a381d3a58b5e443d154ad90620e
Frederic Bastian <frederic.bastian@isb-sib.ch>
2014-12-21 16:52:40 +0000

    add google analytics

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
@@ -138,6 +138,16 @@ public class HtmlParentDisplay extends ConcreteDisplayParent
         this.writeln("</div>");
         this.writeln("</footer>");
         this.writeln("</div>");
+        
+        this.writeln("<script>");
+        this.writeln("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+        this.writeln("(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+        this.writeln("m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+        this.writeln("})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+        this.writeln("ga('create', 'UA-18281910-2', 'auto');");
+        this.writeln("ga('send', 'pageview');");
+        this.writeln("</script>");
+        
         this.writeln("</body>");
         this.writeln("</html>");
         log.exit();

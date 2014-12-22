e1bd19572948ab4d4fe457091d915b342e89ca99
Frederic Bastian <frederic.bastian@isb-sib.ch>
2014-12-22 02:42:44 +0000

    put async google analytics script in head tag

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
@@ -113,7 +113,18 @@ public class HtmlParentDisplay extends ConcreteDisplayParent
                            // (views must override this method if needed)
         this.includeJs();  // load default js files, and css files specific of a view 
                            // (views must override this method if needed)
+        //google analytics
+        this.writeln("<script>");
+        this.writeln("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
+        this.writeln("(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
+        this.writeln("m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
+        this.writeln("})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
+        this.writeln("ga('create', 'UA-18281910-2', 'auto');");
+        this.writeln("ga('send', 'pageview');");
+        this.writeln("</script>");
+        
         this.writeln("</head>");
+        
         this.writeln("<body>");
         this.writeln("<noscript>Sorry, your browser does not support JavaScript!</noscript>");
         this.writeln("<div id='bgee_top'><a id='TOP'></a></div>");
@@ -138,15 +149,6 @@ public class HtmlParentDisplay extends ConcreteDisplayParent
         this.writeln("</div>");
         this.writeln("</footer>");
         this.writeln("</div>");
-        
-        this.writeln("<script>");
-        this.writeln("(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){");
-        this.writeln("(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),");
-        this.writeln("m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)");
-        this.writeln("})(window,document,'script','//www.google-analytics.com/analytics.js','ga');");
-        this.writeln("ga('create', 'UA-18281910-2', 'auto');");
-        this.writeln("ga('send', 'pageview');");
-        this.writeln("</script>");
         
         this.writeln("</body>");
         this.writeln("</html>");

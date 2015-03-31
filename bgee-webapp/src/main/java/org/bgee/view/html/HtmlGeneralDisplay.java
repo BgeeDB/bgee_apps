3c54be75812ac6716d6b17dae004306172ed449c
Valentine Rech de Laval <valentine.rechdelaval@isb-sib.ch>
2015-03-31 07:37:14 +0000

    Try image instead of icons in home page.

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneralDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneralDisplay.java
@@ -73,17 +73,29 @@ public class HtmlGeneralDisplay extends HtmlParentDisplay implements GeneralDisp
         
         RequestParameters urlDownloadGenerator = this.getNewRequestParameters();
         urlDownloadGenerator.setPage(RequestParameters.PAGE_DOWNLOAD);
-        
-        this.writeln("<div id='feature_list'>");
-        
-        this.writeln("<a href='" + urlDownloadGenerator.getRequestURL() + "' title='Bgee download page'>" +
-        		"<figure><div><img class='pageimg' src='" + this.prop.getImagesRootDirectory() + 
-        		"download.png' alt='Download page screenshot' /></div>" +
-                "<figcaption>Data download</figcaption>" +
-                "</figure></a>");
-        
         RequestParameters urlDocGenerator = this.getNewRequestParameters();
         urlDocGenerator.setPage(RequestParameters.PAGE_DOWNLOAD);
+
+        this.writeln("<div id='feature_list'>");
+        //TODO choose between icons or images
+        this.writeln("<a href='" + urlDownloadGenerator.getRequestURL() + "' title='Bgee download page'>" +
+                "<figure><div><img class='pageimg' src='" + this.prop.getImagesRootDirectory() + 
+                "patchwork.png' alt='Download page screenshot' /></div>" +
+                "<figcaption>Data download</figcaption>" +
+                "</figure></a>");
+        this.writeln("<a href='" + urlDocGenerator.getRequestURL() + "' title='Bgee documentation page'>" +
+                "<figure><div><img class='pageimg' src='" + this.prop.getImagesRootDirectory() + 
+                "books.png' alt='Documentation page screenshot' /></div>" +
+                "<figcaption>Documentation</figcaption>" +
+                "</figure></a>");
+        this.writeln("</div>");
+
+        this.writeln("<div id='feature_list'>");
+        this.writeln("<a href='" + urlDownloadGenerator.getRequestURL() + "' title='Bgee download page'>" +
+                "<figure><div><img class='pageimg' src='" + this.prop.getImagesRootDirectory() + 
+                "download.png' alt='Download page screenshot' /></div>" +
+                "<figcaption>Data download</figcaption>" +
+                "</figure></a>");
         this.writeln("<a href='" + urlDocGenerator.getRequestURL() + "' title='Bgee documentation page'>" +
                 "<figure><div><img class='pageimg' src='" + this.prop.getImagesRootDirectory() + 
                 "documentation.png' alt='Documentation page screenshot' /></div>" +

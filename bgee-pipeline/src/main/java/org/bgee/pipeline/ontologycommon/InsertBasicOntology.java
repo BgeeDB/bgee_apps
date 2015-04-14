dcfe27a551122a203d942a4b9aafc14e247a30d6
Frederic Bastian <frederic.bastian@isb-sib.ch>
2015-04-14 01:00:37 +0000

    implement CIO insertion and unit test

---
+++ bgee-pipeline/src/main/java/org/bgee/pipeline/ontologycommon/InsertBasicOntology.java
@@ -0,0 +1,149 @@
+package org.bgee.pipeline.ontologycommon;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.ConfidenceLevel;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.EvidenceConcordance;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.EvidenceTypeConcordance;
+import org.bgee.model.dao.mysql.connector.MySQLDAOManager;
+import org.bgee.pipeline.MySQLDAOUser;
+import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
+
+/**
+ * This class is used to insert into the database various ontologies, 
+ * with no complex operations required (as opposed to, for instance 
+ * {@link org.bgee.pipeline.uberon.InsertUberon InsertUberon}).
+ * 
+ * @author Frederic Bastian
+ * @version Bgee 13 Apr. 2015
+ * @since Bgee 13
+ */
+public class InsertBasicOntology extends MySQLDAOUser {
+    
+    private final static Logger log = LogManager.getLogger(InsertBasicOntology.class.getName());
+    
+    /**
+     * Default constructor. 
+     */
+    public InsertBasicOntology() {
+        this(null);
+    }
+    /**
+     * Constructor providing the {@code MySQLDAOManager} that will be used by 
+     * this object to perform queries to the database. This is useful for unit testing.
+     * 
+     * @param manager   The {@code MySQLDAOManager} to use.
+     */
+    public InsertBasicOntology(MySQLDAOManager manager) {
+        super(manager);
+    }
+    
+    /**
+     * Insert the provided CIO ontology into the database.
+     * 
+     * @param cioOnt    An {@code OWLOntology} that is the CIO ontology.
+     * @throws IllegalArgumentException If the provided CIO ontology was invalid and 
+     *                                  did not allow to retrieve some required information.
+     */
+    public void insertCIO(OWLOntology cioOnt) {
+        log.entry(cioOnt);
+        log.info("Start inserting CIO into database...");
+
+        CIOWrapper cioWrapper = new CIOWrapper(cioOnt);
+        try {
+            this.startTransaction();
+            
+            this.getCIOStatementDAO().insertCIOStatements(this.getCIOTOs(cioWrapper));
+            
+            this.commit();
+        } finally {
+            this.closeDAO();
+        }
+        
+
+        log.info("Done inserting CIO into database.");
+        log.exit();
+    }
+    
+    /**
+     * Extract from the provided CIO all valid terms that are CIO statements 
+     * usable for annotations and return them as a {@code Set} of {@code CIOStatementTO}s.
+     * 
+     * @param cioWrapper    A {@code CIOWrapper} wrapping the CIO Ontology.
+     * @return              A {@code Set} of {@code CIOStatementTO}s corresponding to 
+     *                      valid terms in the ontology wrapped by {@code cioWrapper}.
+     * @throws IllegalArgumentException If the provided CIO ontology was invalid and 
+     *                                  did not allow to retrieve some required information.
+     */
+    private Set<CIOStatementTO> getCIOTOs(CIOWrapper cioWrapper) 
+            throws IllegalArgumentException {
+        log.entry(cioWrapper);
+        
+        Set<CIOStatementTO> cioTOs = new HashSet<CIOStatementTO>();
+        
+        for (OWLClass cls: cioWrapper.getOWLGraphWrapper().getAllOWLClasses()) {
+            log.trace("Examining class {}", cls);
+            //we only insert CIO statements, and only CIO statements that either have 
+            //a confidence level, or that are the strongly conflicting statements, 
+            //with no confidence level
+            if (!cioWrapper.isConfidenceStatement(cls) || 
+                    (!cioWrapper.hasLeafConfidenceLevel(cls) && 
+                            !cioWrapper.isStronglyConflicting(cls))) {
+                log.trace("Not a valid CIO statement, discarded.");
+                continue;
+            }
+            //retrieve information for insertion
+            
+            String id = cioWrapper.getOWLGraphWrapper().getIdentifier(cls);
+            String label = cioWrapper.getOWLGraphWrapper().getLabel(cls);
+            String description = cioWrapper.getOWLGraphWrapper().getDef(cls);
+            if (StringUtils.isBlank(id) || StringUtils.isBlank(label) || 
+                    StringUtils.isBlank(description)) {
+                throw log.throwing(new IllegalArgumentException("Missing ID, label "
+                        + "or description for a class in the provided ontology, "
+                        + "offending class: " + cls));
+            }
+            
+            boolean isTrusted = !cioWrapper.isBgeeNotTrustedStatement(cls);
+            
+            //confidence level
+            //there is none for strongly conflicting statements
+            ConfidenceLevel confLevel = null;
+            if (!cioWrapper.isStronglyConflicting(cls)) {
+                confLevel = ConfidenceLevel.convertToConfidenceLevel(
+                         cioWrapper.getOWLGraphWrapper().getLabel(
+                                 cioWrapper.getConfidenceLevel(cls)));
+            }
+            
+            OWLClass evidenceConcordCls = cioWrapper.getEvidenceConcordance(cls);
+            EvidenceConcordance evidenceConcordance = 
+                    EvidenceConcordance.convertToEvidenceConcordance(
+                            cioWrapper.getOWLGraphWrapper().getLabel(evidenceConcordCls));
+            
+            //evidence type concordance
+            //there is none for single evidence statements
+            EvidenceTypeConcordance evidenceTypeConcordance = null;
+            if (!cioWrapper.isSingleEvidenceConcordance(evidenceConcordCls)) {
+                evidenceTypeConcordance = 
+                        EvidenceTypeConcordance.convertToEvidenceTypeConcordance(
+                            cioWrapper.getOWLGraphWrapper().getLabel(
+                                    cioWrapper.getEvidenceTypeConcordance(cls)));
+            }
+            
+            cioTOs.add(new CIOStatementTO(id, label, description, isTrusted, confLevel, 
+                    evidenceConcordance, evidenceTypeConcordance));
+        }
+        
+        if (cioTOs.isEmpty()) {
+            throw log.throwing(new IllegalArgumentException("The provided ontology "
+                    + "did not allow to retrieve any valid CIO statements."));
+        }
+        return log.exit(cioTOs);
+    }
+}
---
+++ bgee-pipeline/src/test/java/org/bgee/pipeline/ontologycommon/InsertBasicOntologyTest.java
@@ -0,0 +1,120 @@
+package org.bgee.pipeline.ontologycommon;
+
+import static org.mockito.Mockito.verify;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.dao.api.TOComparator;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.ConfidenceLevel;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.EvidenceConcordance;
+import org.bgee.model.dao.api.ontologycommon.CIOStatementDAO.CIOStatementTO.EvidenceTypeConcordance;
+import org.bgee.pipeline.TestAncestor;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.obolibrary.oboformat.parser.OBOFormatParserException;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+/**
+ * Tests for {@link InsertBasicOntology}.
+ * 
+ * @author Frederic Bastian
+ * @version Bgee 13 Apr. 2015
+ * @since Bgee 13
+ */
+public class InsertBasicOntologyTest extends TestAncestor {
+    /**
+     * {@code Logger} of the class. 
+     */
+    private final static Logger log = 
+            LogManager.getLogger(InsertBasicOntologyTest.class.getName());
+    
+    /**
+     * Default Constructor. 
+     */
+    public InsertBasicOntologyTest() {
+        super();
+    }
+    @Override
+    protected Logger getLogger() {
+        return log;
+    }
+    
+    /**
+     * Test {@link InsertBasicOntology#insertCIO(OWLOntology)}
+     */
+    //warnings raised because of the mockito argument captor, discard
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void shouldInsertCIO() throws OBOFormatParserException, 
+    OWLOntologyCreationException, IOException {
+        MockDAOManager mockManager = new MockDAOManager();
+        InsertBasicOntology insert = new InsertBasicOntology(mockManager);
+        
+        Set<CIOStatementTO> expectedTOs = new HashSet<CIOStatementTO>();
+        expectedTOs.add(new CIOStatementTO("CIO:0000003", 
+                "high confidence from single evidence", 
+                "A confidence statement from single evidence related to assertions "
+                + "supported by a highly reliable evidence.", 
+                true, ConfidenceLevel.HIGH_CONFIDENCE, EvidenceConcordance.SINGLE_EVIDENCE, 
+                null));
+        expectedTOs.add(new CIOStatementTO("CIO:0000010", 
+                "confidence statement from strongly conflicting evidence lines of multiple types", 
+                "A confidence statement from conflicting evidence lines of multiple types, "
+                + "where evidence lines are in strong contradiction, meaning that "
+                + "the supporting evidence lines yield different conclusion, several of "
+                + "them being equally likely to be true.", 
+                false, null, EvidenceConcordance.STRONGLY_CONFLICTING, 
+                EvidenceTypeConcordance.DIFFERENT_TYPE));
+        expectedTOs.add(new CIOStatementTO("CIO:0000013", 
+                "confidence statement from congruent evidence lines of multiple types, "
+                + "overall confidence medium", 
+                "A confidence statement from congruent evidence lines of multiple types, "
+                + "of an overall moderately trusted confidence level.", 
+                true, ConfidenceLevel.MEDIUM_CONFIDENCE, EvidenceConcordance.CONGRUENT, 
+                EvidenceTypeConcordance.DIFFERENT_TYPE));
+        expectedTOs.add(new CIOStatementTO("CIO:0000018", 
+                "confidence statement from congruent evidence lines of same type, "
+                + "overall confidence low", 
+                "A confidence statement from congruent evidence lines of same type, "
+                + "of an overall not-trusted confidence level.", 
+                true, ConfidenceLevel.LOW_CONFIDENCE, EvidenceConcordance.CONGRUENT, 
+                EvidenceTypeConcordance.SAME_TYPE));
+        expectedTOs.add(new CIOStatementTO("CIO:0000020", 
+                "confidence statement from strongly conflicting evidence lines of same type", 
+                "Confidence statement from conflicting evidence lines of same type, "
+                + "where evidence lines are in strong contradiction, meaning that "
+                + "the supporting evidence lines yield different conclusion, several "
+                + "of them being equally likely to be true.", 
+                false, null, EvidenceConcordance.STRONGLY_CONFLICTING, 
+                EvidenceTypeConcordance.SAME_TYPE));
+        expectedTOs.add(new CIOStatementTO("CIO:0000023", 
+                "confidence statement from weakly conflicting evidence lines of same type, "
+                + "overall confidence low", 
+                "Confidence statement from weakly conflicting evidence lines of same type, "
+                + "of an overall not-trusted confidence level.", 
+                false, ConfidenceLevel.LOW_CONFIDENCE, EvidenceConcordance.WEAKLY_CONFLICTING, 
+                EvidenceTypeConcordance.SAME_TYPE));
+        expectedTOs.add(new CIOStatementTO("CIO:0000025", 
+                "confidence statement from weakly conflicting evidence lines of multiple types, "
+                + "overall confidence high", 
+                "Confidence statement from weakly conflicting evidence lines of multiple types, "
+                + "of an overall high confidence level.", 
+                true, ConfidenceLevel.HIGH_CONFIDENCE, EvidenceConcordance.WEAKLY_CONFLICTING, 
+                EvidenceTypeConcordance.DIFFERENT_TYPE));
+
+        insert.insertCIO(OntologyUtils.loadOntology(
+                this.getClass().getResource("/ontologies/test_insert_cio.obo").getFile()));
+        
+        ArgumentCaptor<Set> cioTermTOsArg = ArgumentCaptor.forClass(Set.class);
+        verify(mockManager.mockCIOStatementDAO).insertCIOStatements(cioTermTOsArg.capture());
+        if (!TOComparator.areTOCollectionsEqual(expectedTOs, cioTermTOsArg.getValue())) {
+            throw new AssertionError("Incorrect CIOStatementTOs generated, " +
+                    "expected " + expectedTOs + ", but was " + cioTermTOsArg.getValue());
+        }
+    }
+}

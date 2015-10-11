97c52ffaa9a06d4053928fb62847c7573d349a7f
fbastian <frederic.bastian@gmail.com>
2015-10-11 22:44:05 +0200

    Fix missing root directory to link to download files, added by JSHelper

--- bgee-webapp/src/main/java/org/bgee/utils/JSHelper.java
+++ bgee-webapp/src/main/java/org/bgee/utils/JSHelper.java
@@ -1,11 +1,15 @@
 package org.bgee.utils;
 
 import com.google.gson.*;
-import org.bgee.model.file.DownloadFile;
-import org.bgee.model.file.SpeciesDataGroup;
-import org.bgee.model.species.Species;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
-import java.lang.annotation.Inherited;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.controller.BgeeProperties;
+import org.bgee.model.file.DownloadFile;
+
+import java.io.IOException;
 import java.lang.reflect.Type;
 
 /**
@@ -20,6 +24,7 @@ import java.lang.reflect.Type;
 //your class is a "real" util class for the webapp.
 public class JSHelper {
 
+    private static final Logger log = LogManager.getLogger(JSHelper.class.getName());
 
     /**
      * The {@code GsonBuilder} used to dump JSON
@@ -28,6 +33,7 @@ public class JSHelper {
 
     static {
         builder = new GsonBuilder();
+        builder.registerTypeAdapter(DownloadFile.class, new DownloadFileTypeAdapter());
         builder.registerTypeAdapter(DownloadFile.CategoryEnum.class, new CategoryEnumSerializer());
         builder.setPrettyPrinting();
     }
@@ -55,4 +61,37 @@ public class JSHelper {
         }
 
     }
+     
+     static final class DownloadFileTypeAdapter extends TypeAdapter<DownloadFile> {
+
+        @Override
+        public void write(JsonWriter out, DownloadFile value) throws IOException {
+            log.entry(out, value);
+            if (value == null) {
+                out.nullValue();
+                log.exit(); return;
+            }
+            out.beginObject();
+            //values with no modifications
+            out.name("name").value(value.getName());
+            out.name("size").value(value.getSize());
+            out.name("speciesDataGroupId").value(value.getSpeciesDataGroupId());
+            //values with modifications
+            //rewrite path to point to root directory of download files
+            out.name("path").value(BgeeProperties.getBgeeProperties().getDownloadRootDirectory() + value.getPath());
+            //write stringRepresentation of Category
+            out.name("category").value(value.getCategory() == null? "": value.getCategory().getStringRepresentation());
+            
+            
+            out.endObject();
+            log.exit();
+        }
+
+        @Override
+        public DownloadFile read(JsonReader in) throws IOException {
+            //for now, we never read JSON values
+            throw log.throwing(new UnsupportedOperationException("No custom JSON reader for DownloadFile."));
+        }
+         
+     }
 }

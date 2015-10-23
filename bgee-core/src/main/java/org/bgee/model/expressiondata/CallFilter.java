fbd2987840a0ab5979daffc68b3acd1edb6577d6
fbastian <frederic.bastian@gmail.com>
2015-10-23 14:49:42 +0200

    Remove TaxonomyFilter/forceHomology fields from CallFilter, this mechanism was too cumbersome. It seems clearer to have different methods in CallService for single-species/multi-species queries. Added example method signatures in CallService.
    As a result, the use of the class CallDataConditionFilter now also seems to be over-complicated.
    => Remove CallDataConditionFilter, and use directly ConditionFilter and CallData in class CallFilter. The only overhead is that it is not possible anymore to provide a same GeneFilter to target several CallDataConditionFilters; We will simpy use the same GeneFilter in several CallFilters...

--- bgee-core/src/main/java/org/bgee/model/expressiondata/CallFilter.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/CallFilter.java
@@ -4,27 +4,36 @@ import java.util.Set;
 
 import org.bgee.model.expressiondata.baseelements.CallType;
 import org.bgee.model.gene.GeneFilter;
-import org.bgee.model.species.TaxonomyFilter;
 
-//XXX: 
-// - if both IDs and multiple species requested AND forceHomology is true 
-//   => find missing orthologous genes/homologous organs/comparable stages
+//XXX: would several CallFilters represent AND or OR conditions.
+//If OR conditions, we could provide a Set<Set<CallFilter>> to CallService methods, 
+//to provide AND/OR conditions.
+//IF AND conditions, then we cannot easily target different CallDatas for different ConditionFilters, e.g.: 
+//((brain adult and affymetrixQual >= high) OR (liver adult and rnaSeqQual >= high). 
+//=> I don't think we'd often like to do such a query, most of the time we would target 
+//the same CallDatas. If we really needed it, then we could still do it in several queries 
+//(even if it is less optimized).
+//=> let's consider several CallFilters as AND conditions for now, and let's see what happens in the future.  
 public class CallFilter {
+    //XXX: The only problem with using directly ConditionFilters and CallDatas in this class, 
+    //is that GeneFilters are costly to use in a query; using the class CallDataConditionFilter 
+    //was allowing to have a same GeneFilter to target several conditions/call data combinations. 
+    //Now, the same query would be doable by using several CallFilters, but with a same GeneFilter 
+    //reused several times. This is costly, but we could have a mechanism to provide a global GeneFilter 
+    //to the DAO when we see it is always the same GeneFilter used. 
+    //I think it's worth it for the simplification it allows in the class CallFilter.
     private final GeneFilter geneFilter;
     
-    //XXX: each CallDataConditionFilter represents an AND condition.
-    private final Set<CallDataConditionFilter> callDataConditionFilters;
-    
-    //XXX: impact both the gene filtering and the anat.entity and stage filtering.
-    //Species should be always explicitly targeted.
-    private final TaxonomyFilter taxonFilter;
-    
-    //XXX: with this boolean set to true, any multi-species query will search explicitly 
-    //for homology/orthology relations, and will complete ID list provided to potentially 
-    //add homolog/orthologs (i.e., impacting both ConditionFilters and GeneFilters).
-    //if false, then any query is possible, without caring about homology/orthology.
-    //XXX: If true, retrieve results only in homologous structure/comparable stages, always.
-    private final boolean forceHomology;
+    //XXX: all parameters are OR conditions
+    private final Set<ConditionFilter> conditionFilters;
+    //XXX: all CallData would be OR conditions.
+    //The only type of query not easily doable would be: 
+    //affymetrixData = expressed high && rnaSeqData = expressed high
+    //XXX: should we force the CallData to be either ExpressionCallData or DiffExpressionCallData 
+    //using generic types? And even, for the ExpressionCallData, should we force the call type 
+    //to be either EXPRESSED or NOT_EXPRESSED. They are not in the same tables, so this would help...
+    //XXX: again, where to accept the diffExpressionFactor
+    private final Set<CallData<? extends CallType>> callDataFilters;
     
     
     //XXX: here, it means that the DataType of CallData will be null. Is it valid? 
@@ -43,8 +52,7 @@ public class CallFilter {
     
     public CallFilter() {
         this.geneFilter = null;
-        this.callDataConditionFilters = null;
-        this.taxonFilter = null;
-        this.forceHomology = true;
+        this.conditionFilters = null;
+        this.callDataFilters = null;
     }
 }
--- bgee-core/src/main/java/org/bgee/model/expressiondata/CallService.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/CallService.java
@@ -1,9 +1,14 @@
 package org.bgee.model.expressiondata;
 
+import java.util.Set;
+import java.util.stream.Stream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.model.Service;
 import org.bgee.model.dao.api.DAOManager;
+import org.bgee.model.expressiondata.baseelements.SummaryCallType;
+import org.bgee.model.species.TaxonomyFilter;
 
 /**
  * A {@link Service} to obtain {@link Call} objects. 
@@ -20,6 +25,23 @@ public class CallService extends Service {
     //(e.g., GENE, ANAT_ENTITY, STAGE, DATA). But this means that we once again 
     //"duplicate" the concepts in the Condition class. 
     
+//******************
+// Notes from previous CallFilter version
+//******************
+  //XXX: 
+ // - if both IDs and multiple species requested AND forceHomology is true 
+ //   => find missing orthologous genes/homologous organs/comparable stages
+    
+    //XXX: impact both the gene filtering and the anat.entity and stage filtering.
+    //Species should be always explicitly targeted.
+//    private final TaxonomyFilter taxonFilter; 
+    //XXX: with this boolean set to true, any multi-species query will search explicitly 
+    //for homology/orthology relations, and will complete ID list provided to potentially 
+    //add homolog/orthologs (i.e., impacting both ConditionFilters and GeneFilters).
+    //if false, then any query is possible, without caring about homology/orthology.
+    //XXX: If true, retrieve results only in homologous structure/comparable stages, always.
+//    private final boolean forceHomology;
+//******************
 
     /**
      * 0-arg constructor that will cause this {@code CallService} to use 
@@ -39,5 +61,24 @@ public class CallService extends Service {
         super(daoManager);
     }
     
+    //XXX: example multi-species query, signature/returned value to be better defined. 
+    //We could then have the calls ordered by OMA HOG IDs (as in the current pipeline), 
+    //to be able to group calls of orthologous genes in homologous organs. I believe 
+    //this CallService should manage the ordering if requested, but not the grouping. 
+    //XXX: or should we have Calls with MultiSpeciesConditions? Then the grouping 
+    //of homologous organs could be performed by the CallService, if provided with the mapping 
+    //of homologous organs as method argument. 
+    public Stream<Call<? extends SummaryCallType, ? extends CallData<?>>> loadCallsInMultiSpecies(
+            TaxonomyFilter taxonFilter, Set<CallFilter> callFilters) {
+        //TODO
+        return null;
+    }
     
+    //XXX: example single-species query, signature/returned value to be better defined
+    //XXX: would several CallFilters represent AND or OR conditions?
+    public Stream<Call<? extends SummaryCallType, ? extends CallData<?>>> loadCalls(
+            String speciesId, Set<CallFilter> callFilters) {
+        //TODO
+        return null;
+    }
 }

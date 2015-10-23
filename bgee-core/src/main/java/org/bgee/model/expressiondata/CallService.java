ef94554a580a01398eff1c40c6d6e1fd66d2a989
fbastian <frederic.bastian@gmail.com>
2015-10-23 12:29:06 +0200

    make CallService a Service and not a QueryTool

--- bgee-core/src/main/java/org/bgee/model/expressiondata/CallService.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/CallService.java
@@ -1,55 +1,42 @@
 package org.bgee.model.expressiondata;
 
-import java.util.ArrayDeque;
-import java.util.Collection;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.bgee.model.Entity;
-import org.bgee.model.QueryTool;
+import org.bgee.model.Service;
+import org.bgee.model.dao.api.DAOManager;
 
 /**
- * This class is extended by query tools, and is responsible for retrieving 
- * raw expression calls using {@code DAO}s. The reason why this class 
- * is {@code package-private}, and is not a query tool on its own, 
- * is that it will return {@code TransferObject}s from the {@code bgee-dao-api} 
- * modules, and we do not want to expose such classes to the public API. These 
- * {@code TransferObject}s will be processed by the child classes, and only 
- * processed results using classes from the {@code bgee-core} module 
- * will be exposed. 
+ * A {@link Service} to obtain {@link Call} objects. 
+ * Users should use the {@link org.bgee.model.ServiceFactory} to obtain {@code CallService}s.
  * 
  * @author Frederic Bastian
- * @version Bgee 13
- * @since Bgee 13
+ * @version Bgee 13 Oct. 2015
+ * @since Bgee 13 Oct. 2015
  */
-//XXX: A Service exposing Calls only?
-public class CallService extends QueryTool {
-	/**
-     * <code>Logger/code> of this class.
-     */
-    private final static Logger log = 
-    		LogManager.getLogger(CallService.class.getName());
+public class CallService extends Service {
+    private final static Logger log = LogManager.getLogger(CallService.class.getName());
     
     //XXX: Enum class for fields of Call to populate? 
     //(e.g., GENE, ANAT_ENTITY, STAGE, DATA). But this means that we once again 
     //"duplicate" the concepts in the Condition class. 
     
-	/**
-	 * Default constructor. 
-	 */
-    protected CallService() {
-    	super();
-    }
 
-    @Override
-    protected Logger getLogger() {
-        return log;
+    /**
+     * 0-arg constructor that will cause this {@code CallService} to use 
+     * the default {@code DAOManager} returned by {@link DAOManager#getDAOManager()}. 
+     * 
+     * @see #CallService(DAOManager)
+     */
+    public CallService() {
+        this(DAOManager.getDAOManager());
+    }
+    /**
+     * @param daoManager    The {@code DAOManager} to be used by this {@code CallService} 
+     *                      to obtain {@code DAO}s.
+     * @throws IllegalArgumentException If {@code daoManager} is {@code null}.
+     */
+    public CallService(DAOManager daoManager) {
+        super(daoManager);
     }
     
     

ceb11dda173a77b4bb6ca0297f49f9954dd2c6ab
fbastian <frederic.bastian@gmail.com>
2015-11-10 02:31:13 +0100

    add HTML from web-team to TopAnat home page

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlTopAnatDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlTopAnatDisplay.java
@@ -51,14 +51,56 @@ public class HtmlTopAnatDisplay extends HtmlParentDisplay implements TopAnatDisp
         log.entry();
         
         this.startDisplay("Bgee TopAnat page");
-
-        this.writeln("<h1>TopAnat</h1>");
-        this.writeln("<h2>Test for Enrichment and Depletion of "
-                + "Gene Expression in Anatomical Structures</h2>");
         
-        this.writeln(this.getForm(true));
-        this.writeln(this.getJobInfo());
-        this.writeln(this.getResults());
+        //AngularJS module container
+        this.writeln("<div ng-app='app'>");
+
+        this.writeln("<!--[if lt IE 7]>" +
+        "<p class='browsehappy'>You are using an <strong>outdated</strong> browser. Please <a href='http://browsehappy.com/'>upgrade your" +
+            "browser</a> to improve your experience.</p>" +
+        "<![endif]-->");
+
+        this.writeln("<div class='container'>" +
+            "<nav class='navbar navbar-default navbar-fixed-top'>" +
+                "<div class='row'>" +
+                    "<div class='col-sm-4'>" +
+                        "<a class='navbar-brand' href='http://bgee.org/' style = 'padding: 15px 0px 8px 20px'>" +
+                            "<img src='images/bgee13_logo.png' width='86' height='32' alt='Bgee - Gene Expression Evolution'>" +
+                        "</a>" +
+                        "<a class='navbar-brand' href='http://www.isb-sib.ch/' style = 'padding: 15px 0px 8px 20px'>" +
+                            "<img src='images/sib_logo_small.gif' width='42' height='32' alt='SIB Swiss Institute of Bioinformatics'>" +
+                        "</a>" +
+                    "</div>" +
+                    "<div class='col-sm-8'>" +
+
+                        "<h2>TopAnat</h2>" +
+
+                        "<div class='row'>" +
+                            "<div class='col-sm-8'>" +
+                                "<span style='font-size: small'>Test for Enrichment and Depletion of Gene Expression in Anatomical Structures</span>" +
+                            "</div>" +
+
+                            "<!-- LINKS -->" +
+                            "<div class='col-sm-4'>" +
+                                "<a href='' style='margin-right: 6px;' class='pull-right'>" +
+                                    "<span style='margin-right: 6px;vertical-align: middle;' class='glyphicon glyphicon-education' aria-hidden='true'></span>Documentation</a>" +
+                                "<a href='' style='margin-right: 6px;' class='pull-right'>" +
+                                    "<span style='margin-right: 6px;vertical-align: middle;' class='glyphicon glyphicon-th-list' aria-hidden='true'></span>Recent jobs</a>" +
+                            "</div>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>" +
+            "</nav>" +
+        "</div>");
+
+        this.writeln("<div style='padding: 90px 0 0 0;margin-left: 20px; margin-right: 20px' ng-view=''>" +
+
+        "</div>");
+
+        //End AngularJS module container
+        this.writeln("</div>");
+
+
         
         this.endDisplay();
 
@@ -110,38 +152,37 @@ public class HtmlTopAnatDisplay extends HtmlParentDisplay implements TopAnatDisp
     protected void includeJs() {
         log.entry();
         super.includeJs();
-        //currently, AngularJS is only used by TopAnat
+        //external libs used only by TopAnat
         this.includeJs("lib/angular.min.js");
         this.includeJs("lib/angular_modules/angular-animate.min.js");
         this.includeJs("lib/angular_modules/angular-cookies.min.js");
-        this.includeJs("lib/angular_modules/angular-file-upload.min.js");
         this.includeJs("lib/angular_modules/angular-messages.min.js");
         this.includeJs("lib/angular_modules/angular-resource.min.js");
         this.includeJs("lib/angular_modules/angular-route.min.js");
         this.includeJs("lib/angular_modules/angular-sanitize.min.js");
         this.includeJs("lib/angular_modules/angular-touch.min.js");
-        this.includeJs("lib/angular_modules/ui_modules/ui-bootstrap-tpls.min.js");
         this.includeJs("lib/angular_modules/ui_modules/ui-grid.min.js");
-        //jQuery modules only used by TopAnat
+        this.includeJs("lib/angular_modules/ui_modules/ui-bootstrap-tpls.min.js");
         this.includeJs("lib/jquery_plugins/bootstrap.min.js");
+        this.includeJs("lib/angular_modules/angular-file-upload.min.js");
         this.includeJs("lib/jquery_plugins/toastr.min.js");
+        
         //TopAnat JS files
         this.includeJs("topanat/topanat.js");
+        this.includeJs("topanat/services/logger.module.js");
+        this.includeJs("topanat/services/logger.js");
         this.includeJs("topanat/controllers/main.js");
         this.includeJs("topanat/services/bgeedataservice.js");
         this.includeJs("topanat/services/bgeejobservice.js");
+        this.includeJs("topanat/services/helpservice.js");
+        this.includeJs("topanat/services/datatypefactory.js");
         this.includeJs("topanat/services/config.js");
         this.includeJs("topanat/services/constants.js");
-        this.includeJs("topanat/services/datatypefactory.js");
-        this.includeJs("topanat/services/helpservice.js");
-        this.includeJs("topanat/services/logger.js");
-        this.includeJs("topanat/services/logger.module.js");
         log.exit();
     }
     @Override
     protected void includeCss() {
         log.entry();
-        super.includeCss();
         //CSS files of AngularJS modules only used by TopAnat
         this.includeCss("lib/angular_modules/ui-grid.min.css");
         //CSS files of jQuery plugins only used by TopAnat
@@ -149,6 +190,8 @@ public class HtmlTopAnatDisplay extends HtmlParentDisplay implements TopAnatDisp
         this.includeCss("lib/jquery_plugins/toastr.min.css");
         //CSS files specific to TopAnat
         this.includeCss("topanat.css");
+        //we need to add the Bgee CSS files at the end, to override CSS file from bootstrap
+        super.includeCss();
         log.exit();
     }
 

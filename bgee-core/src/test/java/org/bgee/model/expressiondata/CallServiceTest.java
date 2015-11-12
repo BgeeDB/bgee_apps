105c8708703abafc67cb8875ee5c5eab413a9f0e
fbastian <frederic.bastian@gmail.com>
2015-11-13 00:24:06 +0100

    add unit tests for CallService

---
+++ bgee-core/src/test/java/org/bgee/model/expressiondata/CallServiceTest.java
@@ -0,0 +1,23 @@
+package org.bgee.model.expressiondata;
+
+import org.bgee.model.TestAncestor;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link CallService}.
+ * 
+ * @author Frederic Bastian
+ * @version Bgee 13 Nov. 2015
+ * @since Bgee 13 Nov. 2015
+ */
+public class CallServiceTest extends TestAncestor {
+
+    /**
+     * Test the method {@link CallService#loadExpressionCalls(String, ExpressionCallFilter, 
+     * Collection, LinkedHashMap)}.
+     */
+    @Test
+    public void shoudLoadExpressionCalls() {
+        
+    }
+}

0fae67a4c0bc16d626e68393c5dfe7991f846709
fbastian <frederic.bastian@gmail.com>
2015-11-22 22:39:58 +0100

    dirty fix of incorrect routing when AngularJS update an URL

--- bgee-webapp/src/main/webapp/js/topanat/services/bgeejobservice.js
+++ bgee-webapp/src/main/webapp/js/topanat/services/bgeejobservice.js
@@ -31,19 +31,23 @@
         function getJobStatus(hash, jobid, full) {
 
             if(!hash || !jobid){
-                // todo return real error object
+                // TODO return real error object. Return null in the mean time, 
+            	// avoid to log an error server side
+            	console.log("Problem, no jobId nor data hash provided.");
+            	return null;
             }
             var url = configuration.mockupUrl;
 
             console.log("jobid: "+jobid);
 
+            var params = "?page=top_anat&ajax=1&action=tracking_job&display_type=json";
             // we don't want geneinfo on jobstatus after submit
             if(full){
-                var params = "?page=top_anat&ajax=1&action=tracking_job&display_type=json&display_rp=1&gene_info=1&data="+hash;
-            } else {
-                var params = "?page=top_anat&ajax=1&action=tracking_job&display_type=json&data="+hash;
+                params += "&display_rp=1&gene_info=1";
+            } 
+            if (hash) {
+            	params += "&data=" + hash;
             }
-
             if(jobid){
                 params += "&job_id="+jobid;
             }
@@ -61,6 +65,12 @@
         }
 
         function getJobData(hash){
+        	if(!hash){
+                // TODO return real error object. Return null in the mean time, 
+            	// avoid to log an error server side
+            	console.log("Problem, no data hash provided.");
+            	return null;
+            }
             var url = configuration.mockupUrl;
             return $http.get(url+"?page=top_anat&gene_info=1&display_rp=1&ajax=1&action=get_results&display_type=json&data="+hash)
                 .then(getJobData);
--- bgee-webapp/src/main/webapp/js/topanat/topanat.js
+++ bgee-webapp/src/main/webapp/js/topanat/topanat.js
@@ -53,10 +53,21 @@ angular
                 resolve:
 
                 {
+                    //FIXME: Of note, when the URL is updated following a form submission, 
+                    //it triggers this routing, but AngularJS can't be aware of  
+                	//the hash and jobid vars
+                    //(see second comment of this answer: http://stackoverflow.com/a/16266601/1768736) 
+                	//This generates an error on our server (query to server 
+                    //with 'undefined' as value for jobid and hash, see log below). 
+                	//The triggering of this routing is not even needed at this point, 
+                	//after form submission there is already a tracking of job advancement activated.
+                	//Is there a way to prevent this routing to be triggered when URL updated 
+                	//following job submission?
                     jobStatus: function($q, $route, bgeejobservice){
                         var deferred = $q.defer();
                         var jobid = $route.current.params.jobid;
                         var hash = $route.current.params.hash;
+                        console.log("Tracking job page, jobId = " + jobid + " - hash = " + hash);
                         bgeejobservice.getJobStatus(hash, jobid, true)
                             .then(function(data, status){
                                 //console.log(data);
@@ -80,6 +91,7 @@ angular
                 resolve:
 
                 {
+                	//FIXME: same problem of routing triggering as described above
                     jobStatus: function($q, $route, bgeejobservice){
                         var deferred = $q.defer();
                         var hash = $route.current.params.hash;

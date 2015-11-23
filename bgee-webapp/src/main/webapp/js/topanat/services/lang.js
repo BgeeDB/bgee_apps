a34077686507735114feca8d96058614876532d4
fbastian <frederic.bastian@gmail.com>
2015-11-23 05:19:43 +0100

    udpate text

--- bgee-webapp/src/main/webapp/js/topanat/services/lang.js
+++ bgee-webapp/src/main/webapp/js/topanat/services/lang.js
@@ -5,7 +5,10 @@ angular.module('services.lang', [])
     .constant('lang', {
         jobProgressStart: 'Processing job, waiting for a job id. Please do not close this window.',
         jobProgress: 'Job has been submitted as jobid: ',
-        jobProgressBookmark: 'After bookmarking this page it\'s safe to close this window.'
+        jobProgressBookmark: 'After bookmarking this page, it is safe to close this window. ' 
+                             + 'Your analysis is being run on our server, and the results will appear '
+                             + 'as soon as available. It is not necessary to refresh this page, ' 
+                             + 'it will be automatically updated.'
 
     });
 

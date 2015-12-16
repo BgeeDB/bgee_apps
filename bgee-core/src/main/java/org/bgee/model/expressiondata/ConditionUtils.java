71c45734103bdc8788ab03777d9ed0b0fb41cc06
fbastian <frederic.bastian@gmail.com>
2015-12-16 16:33:20 +0100

    implement ConditionUtils, plus method for convenience in Condition

---
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/ConditionUtils.java
@@ -0,0 +1,133 @@
+package org.bgee.model.expressiondata;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.ServiceFactory;
+import org.bgee.model.anatdev.AnatEntity;
+import org.bgee.model.anatdev.DevStage;
+import org.bgee.model.ontology.Ontology;
+import org.bgee.model.ontology.OntologyService;
+
+/**
+ * Class providing common operations on {@link Condition}s.
+ * 
+ * @author Frederic Bastian
+ * @version Bgee 13 Dec. 2015
+ * @since Bgee 13 Dec. 2015
+ */
+public class ConditionUtils {
+
+    private static final Logger log = LogManager.getLogger(ConditionUtils.class.getName());
+    
+    /**
+     * @see #getConditions()
+     */
+    private final Set<Condition> conditions;
+    /**
+     * A {@code ServiceFactory} allowing to acquire the {@code Service}s necessary to {@code ConditionUtils}.
+     */
+    private final ServiceFactory serviceFactory;
+    
+    /**
+     * @see #getAnatEntityOntology()
+     */
+    private final Ontology<AnatEntity> anatEntityOnt;
+    /**
+     * @see #getDevStageOntology()
+     */
+    private final Ontology<DevStage> devStageOnt;
+    
+    public ConditionUtils(Collection<Condition> conditions, ServiceFactory serviceFactory) 
+            throws IllegalArgumentException {
+        if (conditions == null || conditions.isEmpty()) {
+            throw log.throwing(new IllegalArgumentException("Some conditions must be provided."));
+        }
+        if (serviceFactory == null) {
+            throw log.throwing(new IllegalArgumentException("A ServiceFactory must be provided."));
+        }
+        
+        this.conditions = new HashSet<>(conditions);
+        this.serviceFactory = serviceFactory;
+        
+        Set<String> anatEntityIds = new HashSet<>();
+        Set<String> devStageIds = new HashSet<>();
+        for (Condition cond: this.conditions) {
+            anatEntityIds.add(cond.getAnatEntityId());
+            devStageIds.add(cond.getDevStageId());
+        }
+        
+        OntologyService ontService = this.serviceFactory.getOntologyService();
+        this.anatEntityOnt = ontService.getAnatEntityOntology(anatEntityIds, 
+                this.serviceFactory.getAnatEntityService());
+        this.devStageOnt = ontService.getDevStageOntology(devStageIds, 
+                this.serviceFactory.getDevStageService());
+    }
+    
+    /**
+     * Determines whether the second condition is more precise than the first condition. 
+     * "More precise" means that the anatomical structure of {@code secondCond} would be a descendant 
+     * of the anatomical structure of {@code firstCond}, and the developmental stage 
+     * of {@code secondCond} would be a descendant of the developmental stage of {@code firstCond}.
+     * 
+     * @param firstCond     The first {@code Condition} to be checked for relations to {@code secondCond}. 
+     * @param secondCond    The second {@code Condition} to be checked for relations to {@code firstCond}. 
+     * @return              {@code true} if {@code secondCond} is more precise than {@code firstCond}.
+     * @throws IllegalArgumentException If one of the provided {@code Condition}s is not registered 
+     *                                  to this {@code ConditionUtils}.
+     */
+    public boolean isConditionMorePrecise(Condition firstCond, Condition secondCond) throws IllegalArgumentException {
+        log.entry(firstCond, secondCond);
+        if (!this.getConditions().contains(firstCond) || !this.getConditions().contains(secondCond)) {
+            throw log.throwing(new IllegalArgumentException("Some of the provided conditions "
+                    + "are not registered to this ConditionUtils. First condition: " + firstCond 
+                    + " - Second condition: " + secondCond));
+        }
+        
+        Set<DevStage> parentDevStages = this.devStageOnt.getAncestors(
+                this.devStageOnt.getElement(secondCond.getDevStageId()));
+        if (parentDevStages.stream().noneMatch(e -> e.getId().equals(firstCond.getDevStageId()))) {
+            return log.exit(false);
+        }
+        
+        Set<AnatEntity> parentAnatEntities = this.anatEntityOnt.getAncestors(
+                this.anatEntityOnt.getElement(secondCond.getAnatEntityId()));
+        if (parentAnatEntities.stream().noneMatch(e -> e.getId().equals(firstCond.getAnatEntityId()))) {
+            return log.exit(false);
+        }
+        
+        return log.exit(true);
+    }
+    
+    //*********************************
+    //  GETTERS/SETTERS
+    //*********************************
+    /**
+     * @return  The {@code Set} of {@code Condition}s to be considered for operations on this {@code ConditionUtils}.
+     */
+    public Set<Condition> getConditions() {
+        return conditions;
+    }
+    /**
+     * @return  An {@code Ontology} of {@code AnatEntity}s used to infer relations between {@code Condition}s. 
+     *          Contains only {@code AnatEntity}s and relations for entities present 
+     *          in the {@code Condition}s provided at instantiation.
+     * @see #getDevStageOntology()
+     */
+    public Ontology<AnatEntity> getAnatEntityOntology() {
+        return anatEntityOnt;
+    }
+    /**
+     * @return  An {@code Ontology} of {@code DevStage}s used to infer relations between {@code Condition}s. 
+     *          Contains only {@code DevStage}s and relations for entities present 
+     *          in the {@code Condition}s provided at instantiation.
+     * @see #getAnatEntityOntology()
+     */
+    public Ontology<DevStage> getDevStageOntology() {
+        return devStageOnt;
+    }
+    
+}
---
+++ bgee-core/src/test/java/org/bgee/model/expressiondata/ConditionUtilsTest.java
@@ -0,0 +1,126 @@
+package org.bgee.model.expressiondata;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.ServiceFactory;
+import org.bgee.model.TestAncestor;
+import org.bgee.model.anatdev.AnatEntity;
+import org.bgee.model.anatdev.AnatEntityService;
+import org.bgee.model.anatdev.DevStage;
+import org.bgee.model.anatdev.DevStageService;
+import org.bgee.model.ontology.Ontology;
+import org.bgee.model.ontology.OntologyService;
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link ConditionUtils}.
+ * 
+ * @author Frederic Bastian
+ * @version Bgee 13 Dec. 2015
+ * @since Bgee 13 Dec. 2015
+ */
+public class ConditionUtilsTest extends TestAncestor {
+    private final static Logger log = LogManager.getLogger(ConditionUtilsTest.class.getName());
+    
+    @Override
+    protected Logger getLogger() {
+        return log;
+    } 
+
+    /**
+     * Test the method {@link ConditionUtils#isConditionMorePrecise(Condition, Condition)}.
+     */
+    @Test
+    public void testIsConditionMorePrecise() {
+        String anatEntityId1 = "anat1";
+        AnatEntity anatEntity1 = new AnatEntity(anatEntityId1);
+        String anatEntityId2 = "anat2";
+        AnatEntity anatEntity2 = new AnatEntity(anatEntityId2);
+        String anatEntityId3 = "anat3";
+        AnatEntity anatEntity3 = new AnatEntity(anatEntityId3);
+        String devStageId1 = "stage1";
+        DevStage devStage1 = new DevStage(devStageId1);
+        String devStageId2 = "stage2";
+        DevStage devStage2 = new DevStage(devStageId2);
+        String devStageId3 = "stage3";
+        DevStage devStage3 = new DevStage(devStageId3);
+        
+        Condition cond1 = new Condition(anatEntityId1, devStageId1);
+        Condition cond2 = new Condition(anatEntityId2, devStageId2);
+        Condition cond3 = new Condition(anatEntityId3, devStageId3);
+        Condition cond4 = new Condition(anatEntityId2, devStageId1);
+        Condition cond5 = new Condition(anatEntityId1, devStageId3);
+        
+        ServiceFactory mockFact = mock(ServiceFactory.class);
+        OntologyService ontService = mock(OntologyService.class);
+        AnatEntityService anatEntityService = mock(AnatEntityService.class);
+        DevStageService devStageService = mock(DevStageService.class);
+        when(mockFact.getOntologyService()).thenReturn(ontService);
+        when(mockFact.getAnatEntityService()).thenReturn(anatEntityService);
+        when(mockFact.getDevStageService()).thenReturn(devStageService);
+        
+        //suppress warning as we cannot specify generic type for a mock
+        @SuppressWarnings("unchecked")
+        Ontology<AnatEntity> anatEntityOnt = mock(Ontology.class);
+        @SuppressWarnings("unchecked")
+        Ontology<DevStage> devStageOnt = mock(Ontology.class);
+        
+        when(ontService.getAnatEntityOntology(
+                new HashSet<>(Arrays.asList(anatEntityId1, anatEntityId2, anatEntityId3)), 
+                anatEntityService))
+        .thenReturn(anatEntityOnt);
+        when(ontService.getDevStageOntology(
+                new HashSet<>(Arrays.asList(devStageId1, devStageId2, devStageId3)), 
+                devStageService))
+        .thenReturn(devStageOnt);
+        
+        when(anatEntityOnt.getElement(anatEntityId1)).thenReturn(anatEntity1);
+        when(anatEntityOnt.getElement(anatEntityId2)).thenReturn(anatEntity2);
+        when(anatEntityOnt.getElement(anatEntityId3)).thenReturn(anatEntity3);
+        when(devStageOnt.getElement(devStageId1)).thenReturn(devStage1);
+        when(devStageOnt.getElement(devStageId2)).thenReturn(devStage2);
+        when(devStageOnt.getElement(devStageId3)).thenReturn(devStage3);
+        
+        when(anatEntityOnt.getAncestors(anatEntity1)).thenReturn(new HashSet<>());
+        when(anatEntityOnt.getAncestors(anatEntity2)).thenReturn(new HashSet<>(Arrays.asList(anatEntity1)));
+        when(anatEntityOnt.getAncestors(anatEntity3)).thenReturn(new HashSet<>(Arrays.asList(anatEntity1)));
+        when(devStageOnt.getAncestors(devStage1)).thenReturn(new HashSet<>());
+        when(devStageOnt.getAncestors(devStage2)).thenReturn(new HashSet<>(Arrays.asList(devStage1)));
+        when(devStageOnt.getAncestors(devStage3)).thenReturn(new HashSet<>(Arrays.asList(devStage1)));
+        
+        ConditionUtils utils = new ConditionUtils(Arrays.asList(cond1, cond2, cond3, cond4, cond5), 
+                mockFact);
+        assertTrue("Incorrect determination of precision for more precise condition", 
+                utils.isConditionMorePrecise(cond1, cond2));
+        assertTrue("Incorrect determination of precision for more precise condition", 
+                utils.isConditionMorePrecise(cond1, cond3));
+        assertFalse("Incorrect determination of precision for less precise condition", 
+                utils.isConditionMorePrecise(cond2, cond1));
+        assertFalse("Incorrect determination of precision for less precise condition", 
+                utils.isConditionMorePrecise(cond3, cond1));
+        assertFalse("Incorrect determination of precision for as precise conditions", 
+                utils.isConditionMorePrecise(cond2, cond3));
+
+        assertFalse("Incorrect determination of precision for condition with anat. entity as precise", 
+                utils.isConditionMorePrecise(cond1, cond5));
+        assertFalse("Incorrect determination of precision for condition with dev. stage as precise", 
+                utils.isConditionMorePrecise(cond1, cond4));
+        
+        //check that an Exception is correctly thrown if a condition used was not provided at instantiation
+        try {
+            utils.isConditionMorePrecise(cond1, new Condition(anatEntityId1, devStageId2));
+            //test fail
+            fail("An exception should be thrown when a Condition was not provided at instantiation.");
+        } catch (IllegalArgumentException e) {
+            //test passed
+        }
+        
+    }
+}

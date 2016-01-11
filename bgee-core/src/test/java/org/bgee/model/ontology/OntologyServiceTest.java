66f66c19176572b068478621c79d7781cbe03d5a
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2016-01-11 23:27:56 +0100

    Start OntologyService unit tests

--- bgee-core/src/test/java/org/bgee/model/ontology/OntologyServiceTest.java
+++ bgee-core/src/test/java/org/bgee/model/ontology/OntologyServiceTest.java
@@ -1,15 +1,24 @@
 package org.bgee.model.ontology;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import org.bgee.model.ServiceFactory;
 import org.bgee.model.TestAncestor;
 import org.bgee.model.anatdev.AnatEntity;
 import org.bgee.model.anatdev.DevStage;
 import org.bgee.model.dao.api.DAOManager;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTO;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTO.RelationStatus;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTO.RelationType;
+import org.bgee.model.dao.api.ontologycommon.RelationDAO.RelationTOResultSet;
 import org.bgee.model.dao.mysql.connector.MySQLDAOManager;
 import org.bgee.model.expressiondata.Condition;
 import org.bgee.model.expressiondata.ConditionUtils;
@@ -30,7 +39,41 @@ public class OntologyServiceTest extends TestAncestor {
      */
     @Test
     public void shouldGetAnatEntityOntology() {
+        DAOManager managerMock = mock(DAOManager.class);
+        RelationDAO dao = mock(RelationDAO.class);
+        when(managerMock.getRelationDAO()).thenReturn(dao);
         
+        RelationTOResultSet mockRelationRs = getMockResultSet(RelationTOResultSet.class, Arrays.asList(
+        		new RelationTO("22", "Anat_id10", "Anat_id5", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("23", "Anat_id11", "Anat_id10", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("12", "Anat_id2", "Anat_id1", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("13", "Anat_id3", "Anat_id2", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("14", "Anat_id4", "Anat_id2", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("15", "Anat_id5", "Anat_id2", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("16", "Anat_id5", "Anat_id7", RelationType.DEVELOPSFROM, RelationStatus.INDIRECT),
+        		new RelationTO("17", "Anat_id5", "Anat_id8", RelationType.DEVELOPSFROM, RelationStatus.DIRECT),
+        		new RelationTO("18", "Anat_id6", "Anat_id1", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("19", "Anat_id7", "Anat_id6", RelationType.DEVELOPSFROM, RelationStatus.DIRECT),
+        		new RelationTO("20", "Anat_id8", "Anat_id7", RelationType.ISA_PARTOF, RelationStatus.DIRECT),
+        		new RelationTO("21", "Anat_id9", "Anat_id5", RelationType.ISA_PARTOF, RelationStatus.DIRECT)));
+        
+        Set<String> speciesIds = new HashSet<String>();
+        speciesIds.addAll(Arrays.asList("11", "22", "44"));
+        
+//        when(dao.getAnatEntityRelations(speciesIds, true, sourceAnatEntityIds, targetAnatEntityIds,
+//                       sourceOrTarget, relationTypes, EnumSet.complementOf(EnumSet.of(RelationStatus.REFLEXIVE)), null)).thenReturn(mockRelationRs);
+//      relationTypes.stream()
+//      .map(Ontology::convertRelationType)
+//      .collect(Collectors.toCollection(() -> 
+//          EnumSet.noneOf(RelationTO.RelationType.class))), 
+
+//        Ontology<AnatEntity> expectedOntology = new Ontology<AnatEntity>(anatEntities, relations, relationTypes);
+//        
+//        OntologyService service = new OntologyService(managerMock);
+//        assertEquals("Incorrect anatomical ontology",
+//                expectedOntology, service.getAnatEntityOntology(speciesIds, anatEntityIds, 
+//                               relationTypes, getAncestors, getDescendants, service));
+     
     }
 
     /**

0341fa45e59caf4457e664818c377e71f71b2ebc
fbastian <frederic.bastian@gmail.com>
2016-01-14 12:01:56 +0100

    fix topanat-web/issues#126

--- bgee-webapp/src/main/webapp/js/topanat/controllers/main.js
+++ bgee-webapp/src/main/webapp/js/topanat/controllers/main.js
@@ -659,6 +659,12 @@
             if (number == undefined) {
                 number = vm.nbRows.selectedOption.id;
             }
+
+            // Issue 126
+            if (number > vm.gridOptions.data.length) {
+                number = vm.gridOptions.data.length;
+            }
+
             // +1 for the header
             var height = (number +1) * vm.row_height;
             angular.element(document.getElementsByClassName('grid')[0]).css('height', height + 'px');
@@ -1175,6 +1181,10 @@
             }
             vm.getFilteredRows(); // In order to get the total number of rows before filtering of results
             vm.jobDone = true; // in order to display the result array
+
+            // Issue 126: Change the default value of the nb of rows to show
+            // if the number of results is less than the default value
+            changeNumberOfRowsToShow(vm.nb_rows);
         }
 
         function displayResults(result) {

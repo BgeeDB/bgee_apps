37fbf84362b33433849351bc863930c80217278b
fbastian <frederic.bastian@gmail.com>
2016-03-16 02:14:42 +0100

    add method to display TransferObjects; implments in CSV view

--- bgee-webapp/src/main/java/org/bgee/view/DAODisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/DAODisplay.java
@@ -1,4 +1,11 @@
 package org.bgee.view;
+
+import java.util.List;
+
+import org.bgee.model.dao.api.DAO;
+import org.bgee.model.dao.api.DAOResultSet;
+import org.bgee.model.dao.api.TransferObject;
+
 /**
  * Interface defining the methods implemented by view rendering results of queries to DAOs, 
  * allowing to use them as a webservice. It is the only view allowed to access some classes 
@@ -9,5 +16,13 @@ package org.bgee.view;
  * @since   Bgee 13
  */
 public interface DAODisplay {
-    
+    /**
+     * Display the {@code TransferObject}s returned by the provided {@code DAOResultSet}. 
+     * Only the fields corresponding to the provided {@Attribute}s are printed. 
+     * 
+     * @param attributes    A {@code List} of {@code DAO.Attribute}s, specifying the fields 
+     *                      of the {@code TransferObject}s to be printed, and their display order.
+     * @param resultSet     A {@code DAOResultSet} to iterate to print {@code TransferObject}s.
+     */
+    public void displayTOs(List<DAO.Attribute> attributes, DAOResultSet<TransferObject> resultSet);
 }
--- bgee-webapp/src/main/java/org/bgee/view/csv/CsvDAODisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/csv/CsvDAODisplay.java
@@ -1,13 +1,21 @@
 package org.bgee.view.csv;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.bgee.controller.BgeeProperties;
 import org.bgee.controller.RequestParameters;
+import org.bgee.model.dao.api.DAO;
+import org.bgee.model.dao.api.DAOResultSet;
+import org.bgee.model.dao.api.TransferObject;
 import org.bgee.view.DAODisplay;
 import org.bgee.view.ViewFactory;
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
 
 /**
  * Implementation of {@code DAODisplay} for CSV rendering.
@@ -17,6 +25,7 @@ import org.bgee.view.ViewFactory;
  * @since   Bgee 13
  */
 public class CsvDAODisplay extends CsvParentDisplay implements DAODisplay {
+    private final static Logger log = LogManager.getLogger(CsvDAODisplay.class.getName());
 
     protected CsvDAODisplay(HttpServletResponse response, RequestParameters requestParameters, 
             BgeeProperties prop, ViewFactory factory, Delimiter delimiter) 
@@ -24,4 +33,25 @@ public class CsvDAODisplay extends CsvParentDisplay implements DAODisplay {
         super(response, requestParameters, prop, factory, delimiter);
     }
     
+    @Override
+    public void displayTOs(List<DAO.Attribute> attributes, DAOResultSet<TransferObject> resultSet) 
+            throws IllegalStateException {
+        log.entry(attributes, resultSet);
+        
+        try (final ICsvBeanWriter beanWriter = new CsvBeanWriter(this.getOut(), this.csvPref)) {
+            String[] header = attributes.stream().map(attr -> attr.toString()).toArray(String[]::new);
+            beanWriter.writeHeader(header);
+            
+            final String[] nameMapping = attributes.stream().map(attr -> attr.getTOFieldName())
+                    .toArray(String[]::new);
+            while (resultSet.next()) {
+                beanWriter.write(resultSet.getTO(), nameMapping);
+            }
+        } catch (IOException e) {
+            log.catching(e);
+            throw log.throwing(new IllegalStateException("Cannot write CSV response", e));
+        }
+        
+        log.exit();
+    }
 }

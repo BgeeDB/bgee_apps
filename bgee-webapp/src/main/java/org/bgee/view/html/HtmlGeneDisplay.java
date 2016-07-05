6935079cb5bcad8c7336283ad38bc99b805422b9
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2016-07-05 15:58:03 +0200

    Remove unnecessary imports

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneDisplay.java
@@ -4,20 +4,17 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.EnumSet;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.SortedMap;
 import java.util.StringJoiner;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.controller.BgeeProperties;

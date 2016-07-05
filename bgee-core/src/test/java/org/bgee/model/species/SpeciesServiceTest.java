9975fc69c21815901f698a8d9574642ea977d07e
Valentine Rech De Laval <valentine.rechdelaval@isb-sib.ch>
2016-07-05 18:28:19 +0200

    Add tests into SpeciesServiceTest

--- bgee-core/src/test/java/org/bgee/model/species/SpeciesServiceTest.java
+++ bgee-core/src/test/java/org/bgee/model/species/SpeciesServiceTest.java
@@ -50,6 +50,7 @@ public class SpeciesServiceTest extends TestAncestor {
                         "version1", "321", null), 
                 new SpeciesTO("1234", "name", "genus", "someSpecies", "1123", "3432241",
                         "versionA", "1321", null));
+        // ResultSet cannot be reused. As we have 2 tests, we need 2 ResultSet
         SpeciesTOResultSet speciesRS = getMockResultSet(SpeciesTOResultSet.class, speciesTos);
         SpeciesTOResultSet speciesRS2 = getMockResultSet(SpeciesTOResultSet.class, speciesTos);
 		when(speciesDAOMock.getSpeciesFromDataGroups()).thenReturn(speciesRS).thenReturn(speciesRS2);
@@ -68,11 +69,11 @@ public class SpeciesServiceTest extends TestAncestor {
 		        new Species("9606", "human", null, "Homo", "sapiens", "version1"), 
 		        new Species("1234", "name", null, "genus", "someSpecies", "versionA")));
 
-		// actual use of the service
 		SourceService sourceService = mock(SourceService.class);
-        when(sourceService.loadAllSources(false)).thenReturn(
-                Arrays.asList(new Source("s1"), new Source("s2"), new Source("s3")));
+		when(sourceService.loadAllSources(false)).thenReturn(
+		        Arrays.asList(new Source("s1"), new Source("s2"), new Source("s3")));
 
+		// actual use of the service
         SpeciesService speciesService = new SpeciesService(managerMock, sourceService);
 		assertEquals(expectedSpecies, speciesService.loadSpeciesInDataGroups(false));
 		
@@ -97,23 +98,56 @@ public class SpeciesServiceTest extends TestAncestor {
 	    // initialize mocks
 	    DAOManager managerMock = mock(DAOManager.class);
 	    SpeciesDAO speciesDAOMock = mock(SpeciesDAO.class);
+        when(managerMock.getSpeciesDAO()).thenReturn(speciesDAOMock);
+        SourceToSpeciesDAO sourceToSpeciesDAOMock = mock(SourceToSpeciesDAO.class);
+        when(managerMock.getSourceToSpeciesDAO()).thenReturn(sourceToSpeciesDAOMock);
 
 	    // mock behavior
+        List<SpeciesTO> speciesTos = Arrays.asList(
+                new SpeciesTO("9606", "human", "Homo", "sapiens", "4312", "3241",
+                        "version1", "321", null), 
+                new SpeciesTO("1234", "name", "genus", "someSpecies", "1123", "3432241",
+                        "versionA", "1321", null));
+        // ResultSet cannot be reused. As we have 2 tests, we need 2 ResultSet
 	    Set<String> speciesIds = new HashSet<>(Arrays.asList("9606", "1234"));
-	    SpeciesTOResultSet resultSetMock = getMockResultSet(SpeciesTOResultSet.class, Arrays.asList(
-	            new SpeciesTO("9606", "human", "Homo", "sapiens", "4312", "3241",
-	                    "version1", "321", null), 
-	            new SpeciesTO("1234", "name", "genus", "someSpecies", "1123", "3432241",
-	                    "versionA", "1321", null)));
-        when(speciesDAOMock.getSpeciesByIds(speciesIds)).thenReturn(resultSetMock);
-	    when(managerMock.getSpeciesDAO()).thenReturn(speciesDAOMock);
+        SpeciesTOResultSet speciesRS = getMockResultSet(SpeciesTOResultSet.class, speciesTos);
+        SpeciesTOResultSet speciesRS2 = getMockResultSet(SpeciesTOResultSet.class, speciesTos);
+        when(speciesDAOMock.getSpeciesByIds(speciesIds)).thenReturn(speciesRS).thenReturn(speciesRS2);
 
+	    SourceToSpeciesTOResultSet sToSpRS = getMockResultSet(SourceToSpeciesTOResultSet.class, 
+	            Arrays.asList(
+	                    new SourceToSpeciesTO("s1", "9606", SourceToSpeciesTO.DataType.EST, InfoType.DATA),
+	                    new SourceToSpeciesTO("s1", "9606", SourceToSpeciesTO.DataType.IN_SITU, InfoType.DATA),
+	                    new SourceToSpeciesTO("s2", "9606", SourceToSpeciesTO.DataType.AFFYMETRIX, InfoType.ANNOTATION),
+	                    new SourceToSpeciesTO("s3", "9606", SourceToSpeciesTO.DataType.RNA_SEQ, InfoType.DATA),
+	                    new SourceToSpeciesTO("s2", "1234", SourceToSpeciesTO.DataType.IN_SITU, InfoType.ANNOTATION)));
+	    when(sourceToSpeciesDAOMock.getSourceToSpecies(null, 
+	            new HashSet<String>(Arrays.asList("9606", "1234")), null, null, null)).thenReturn(sToSpRS);
+
+	    SourceService sourceService = mock(SourceService.class);
+	    when(sourceService.loadAllSources(false)).thenReturn(
+	            Arrays.asList(new Source("s1"), new Source("s2"), new Source("s3")));
+
+	    // actual use of the service
+	    SpeciesService service = new SpeciesService(managerMock, sourceService);
 	    Set<Species> expected = new HashSet<>(Arrays.asList(
 	            new Species("9606", "human", null, "Homo", "sapiens", "version1"),
 	            new Species("1234", "name", null, "genus", "someSpecies", "versionA")));
-
-	    // actual use of the service
-	    SpeciesService service = new SpeciesService(managerMock);
         assertEquals(expected, service.loadSpeciesByIds(speciesIds, false));
+        
+        Map<Source, Set<DataType>> forData9606 = new HashMap<>();
+        forData9606.put(new Source("s1"), new HashSet<DataType>(Arrays.asList(DataType.EST, DataType.IN_SITU)));
+        forData9606.put(new Source("s3"), new HashSet<DataType>(Arrays.asList(DataType.RNA_SEQ)));
+        Map<Source, Set<DataType>> forAnnot9606 = new HashMap<>();
+        forAnnot9606.put(new Source("s2"), new HashSet<DataType>(Arrays.asList(DataType.AFFYMETRIX)));
+        Map<Source, Set<DataType>> forAnnot1234 = new HashMap<>();
+        forAnnot1234.put(new Source("s2"), new HashSet<DataType>(Arrays.asList(DataType.IN_SITU)));
+        expected.clear();
+        expected.add(new Species("9606", "human", null, "Homo", "sapiens", "version1", 
+                forData9606, forAnnot9606));
+        expected.add(new Species("1234", "name", null, "genus", "someSpecies", "versionA", 
+                null, forAnnot1234));
+
+        assertEquals(expected, service.loadSpeciesByIds(speciesIds, true));
 	}
 }

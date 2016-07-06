BgeeDB/bgee_apps

commit efeb06b2dbaf2953ab77c21de5799a87bee71382

Remove setters in Gene class

bgee-core/src/main/java/org/bgee/model/gene/GeneService.java
@@ -27,7 +27,7 @@ import org.bgee.model.species.SpeciesService;
  * @author  Philippe Moret
  * @author  Frederic Bastian
  * @author  Valentine Rech de Laval
- * @version Bgee 13, July 2015
+ * @version Bgee 13, July 2016
  * @since   Bgee 13, Sept. 2015
  */
 public class GeneService extends Service {
@@ -108,7 +108,7 @@ public class GeneService extends Service {
     	assert gene.getSpeciesId() != null;
     	speciesIds.add(gene.getSpeciesId());
     	Species species = speciesService.loadSpeciesByIds(speciesIds, true).iterator().next();
-    	gene.setSpecies(species);
+    	gene = new Gene(gene.getId(), gene.getSpeciesId(), gene.getName(), gene.getDescription(), species);
 		return log.exit(gene);  		
     }
 
@@ -135,7 +135,7 @@ public class GeneService extends Service {
         Set<String> geneIds = matchedGeneList.stream().map(Gene::getId).collect(Collectors.toSet());
         
         for (Gene g: matchedGeneList) {
-            g.setSpecies(speciesMap.get(g.getSpeciesId()));
+            g = new Gene(g.getId(), g.getSpeciesId(), g.getName(), g.getDescription(), speciesMap.get(g.getSpeciesId()));
         }
 
         final Map<String, List<String>> synonymMap = getDaoManager().getGeneNameSynonymDAO().getGeneNameSynonyms(geneIds)

bgee-core/src/main/java/org/bgee/model/gene/Gene.java
@@ -15,7 +15,7 @@ public class Gene extends NamedEntity {
 	/**
 	 * The {@code Species} this {@code Gene} belongs to.
 	 */
-	private Species species;
+	private final Species species;
 	
 	/**
 	 * A {@code String} representing the ID of the {@code Species} 
@@ -23,42 +23,40 @@ public class Gene extends NamedEntity {
 	 * should correspond to the value returned by the {@code getId()} method 
 	 * on this {@code Species} object.
 	 */
-	private String speciesId;
+	private final String speciesId;
 	
 	/**
      * Constructor providing the {@code id} of this {@code Gene}.
      * This {@code id} cannot be {@code null}, or blank,
      * otherwise an {@code IllegalArgumentException} will be thrown.
      * 
-     * @param id    A {@code String} representing the ID of this object.
+     * @param id            A  {@code String} representing the ID of this object.
      * @throws IllegalArgumentException If {@code id} is {@code null}, or blank. 
      */
     public Gene (String id) throws IllegalArgumentException {
-    	super(id);
-    	this.setSpecies(null);
-    	this.setSpeciesId(null);
+    	this(id, null);
     }
     /**
      * Constructor providing the {@code id} of this {@code Gene} and of the species.
      * This {@code id} cannot be {@code null}, or blank,
      * otherwise an {@code IllegalArgumentException} will be thrown.
      * 
-     * @param id        A {@code String} representing the ID of this object.
-     * @param speciesId A {@code String} representing the ID of the species.
+     * @param id            A  {@code String} representing the ID of this object.
+     * @param speciesId     A {@code String} representing the ID of the species this gene belongs to.
      * @throws IllegalArgumentException     if {@code id} is {@code null}, or blank.
      */
     public Gene (String id, String speciesId) throws IllegalArgumentException {
-        super(id);
-        this.setSpecies(null);
-        this.setSpeciesId(speciesId);
+        this(id, null, null);
     }
 
     /**
-     * Constructor providing the {@code id} of this {@code Gene} and of the species. 
+     * Constructor providing the {@code id} of this {@code Gene} and of the species, and the name. 
      * This {@code id} cannot be {@code null}, or blank,
      * otherwise an {@code IllegalArgumentException} will be thrown.
-     * @param id        A {@code String} representing the ID of this object.
-     * @param speciesId A {@code String} representing the ID of the species.
+     * 
+     * @param id            A  {@code String} representing the ID of this object.
+     * @param speciesId     A {@code String} representing the ID of the species this gene belongs to.
+     * @param name          A {@code String} representing the name of this gene.
      * @throws IllegalArgumentException     if {@code id} is {@code null}, or blank.
      */
     public Gene(String id, String speciesId, String name) throws IllegalArgumentException {
@@ -66,19 +64,38 @@ public class Gene extends NamedEntity {
     }
     
     /**
-     * Constructor providing the {@code id} of this {@code Gene} and of the species. 
+     * Constructor providing the {@code id} of this {@code Gene} and of the species, the name, and the description. 
      * This {@code id} cannot be {@code null}, or blank,
      * otherwise an {@code IllegalArgumentException} will be thrown.
-     * @param id        A {@code String} representing the ID of this object.
-     * @param speciesId A {@code String} representing the ID of the species.
-     * @param description A {@code String} representing the description of the gene.
+     * 
+     * @param id            A  {@code String} representing the ID of this object.
+     * @param speciesId     A {@code String} representing the ID of the species this gene belongs to.
+     * @param name          A {@code String} representing the name of this gene.
+     * @param description   A {@code String} representing the description of this gene.
      * @throws IllegalArgumentException     if {@code id} is {@code null}, or blank.
      */
     public Gene(String id, String speciesId, String name, String description) throws IllegalArgumentException {
-    	super(id, name, description);
-    	this.setSpeciesId(speciesId);
+    	this(id, speciesId, name, description, null);
     }
     
+    /**
+     * Constructor providing the {@code id} of this {@code Gene} and of the species, the name,
+     * the description, and the {@code Species}. 
+     * This {@code id} cannot be {@code null}, or blank,
+     * otherwise an {@code IllegalArgumentException} will be thrown.
+     * 
+     * @param id            A  {@code String} representing the ID of this object.
+     * @param speciesId     A {@code String} representing the ID of the species this gene belongs to.
+     * @param name          A {@code String} representing the name of this gene.
+     * @param description   A {@code String} representing the description of this gene.
+     * @param species       A {@code Species} representing the species this gene belongs to.
+     * @throws IllegalArgumentException     if {@code id} is {@code null}, or blank.
+     */
+    public Gene(String id, String speciesId, String name, String description, Species species) throws IllegalArgumentException {
+        super(id, name, description);
+        this.speciesId = speciesId;
+        this.species = species;
+    }
     
 	/**
 	 * Returns the {@code Species} this {@code Gene} belongs to.
@@ -86,18 +103,6 @@ public class Gene extends NamedEntity {
 	 */
 	public Species getSpecies() {
 		return this.species;
-	}
-	/**
-	 * Sets the {@code Species} this {@code Gene} belongs to. 
-	 * <p>
-	 * Following calls to {@link #getSpeciesId()} will return the value returned 
-	 * by a call to {@code getId()} on {@code species}, 
-	 * even if {@link #setSpeciesId(String)} is used previously or afterwards. 
-	 * 
-	 * @param species 	the {@code Species} to set, that this {@code Gene} belongs to.
-	 */
-	public void setSpecies(Species species) {
-		this.species = species;
 	}
 	
 	/**
@@ -121,22 +126,6 @@ public class Gene extends NamedEntity {
 		return this.speciesId;
 	}
 	
-	/**
-	 * Sets the ID of the species this {@code Gene} belongs to. 
-	 * <p>
-	 * Note that the value returned by {@link #getSpeciesId()} might not correspond 
-	 * to the value set here: if {@link #getSpecies()} returns 
-	 * a {@code Species} object not null, then the value set here is not used, 
-	 * and only the value returned by a call to {@code getId()} 
-	 * on the {@code Species} object is used. 
-	 * 
-	 * @param speciesId 	A {@code String} corresponding to the ID of the species 
-	 * 						this {@code Gene} belongs to. 
-	 */
-	public void setSpeciesId(String speciesId) {
-		this.speciesId = speciesId;
-	}
-
 	@Override
 	public int hashCode() {
 	    final int prime = 31;

commit 53fc034fccb11fb7d7bdb56aa9dea0a8d46c538b

fix bug introduced in efeb06b2dbaf2; misc changes along debugging

bgee-core/src/main/java/org/bgee/model/gene/GeneMatch.java
@@ -4,6 +4,8 @@ package org.bgee.model.gene;
  * This class encapsulates a result to a gene search. A match can either be on the gene name
  * or a synonym. 
  * @author Philippe Moret
+ * @author Frederic Bastian
+ * @version Bgee 13 July 2016
  *
  */
 public class GeneMatch {
@@ -37,4 +39,51 @@ public class GeneMatch {
 	public String getMatchedSynonym() {
 		return synonym;
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((gene == null) ? 0 : gene.hashCode());
+        result = prime * result + ((synonym == null) ? 0 : synonym.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        GeneMatch other = (GeneMatch) obj;
+        if (gene == null) {
+            if (other.gene != null) {
+                return false;
+            }
+        } else if (!gene.equals(other.gene)) {
+            return false;
+        }
+        if (synonym == null) {
+            if (other.synonym != null) {
+                return false;
+            }
+        } else if (!synonym.equals(other.synonym)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("GeneMatch [gene=").append(gene).append(", synonym=").append(synonym).append("]");
+        return builder.toString();
+    }
+	
+	
 }

bgee-core/src/main/java/org/bgee/model/gene/GeneService.java
@@ -117,6 +117,7 @@ public class GeneService extends Service {
      * @param term A {@code String} containing the query 
      * @return A {@code List} of results (ordered).
      */
+    //XXX: is it really needed to create Genes twice?...
     public List<GeneMatch> searchByTerm(final String term) {
         log.entry(term);
         GeneDAO dao = getDaoManager().getGeneDAO();
@@ -134,9 +135,10 @@ public class GeneService extends Service {
         
         Set<String> geneIds = matchedGeneList.stream().map(Gene::getId).collect(Collectors.toSet());
         
-        for (Gene g: matchedGeneList) {
-            g = new Gene(g.getId(), g.getSpeciesId(), g.getName(), g.getDescription(), speciesMap.get(g.getSpeciesId()));
-        }
+        //give Species objects to new Gene objects
+        matchedGeneList = matchedGeneList.stream().map(g -> new Gene(g.getId(), g.getSpeciesId(), 
+                g.getName(), g.getDescription(), speciesMap.get(g.getSpeciesId())))
+                .collect(Collectors.toList());
 
         final Map<String, List<String>> synonymMap = getDaoManager().getGeneNameSynonymDAO().getGeneNameSynonyms(geneIds)
                 .stream()

bgee-webapp/src/main/java/org/bgee/view/xml/XmlSearchDisplay.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.controller.BgeeProperties;
@@ -60,7 +61,10 @@ public class XmlSearchDisplay extends XmlParentDisplay implements SearchDisplay 
         			"label='" + XmlParentDisplay.xmlEntities(label) + "' " +
         			"label_source='" + labelSource + "' " +
         			"species_id='" + XmlParentDisplay.xmlEntities(gene.getGene().getSpeciesId()) + "' " +
-        			"species_name='" + XmlParentDisplay.xmlEntities(gene.getGene().getSpecies().getName())+"' "+ 
+        			"species_name='" + XmlParentDisplay.xmlEntities(
+        			        StringUtils.isNotBlank(gene.getGene().getSpecies().getName())? 
+        			                gene.getGene().getSpecies().getName(): 
+        			                gene.getGene().getSpecies().getShortName()) +"' "+ 
         			"/>");
         }
 		this.writeln("</genes>");

bgee-core/src/test/java/org/bgee/model/ontology/OntologyTest.java
@@ -36,7 +36,6 @@ public class OntologyTest extends TestAncestor {
 
         Ontology<AnatEntity> ontology = new Ontology<>(elements, relations, 
                 EnumSet.allOf(Ontology.RelationType.class));
-        System.out.println(ontology);
         
         Set<AnatEntity> ancestors = ontology.getAncestors(ae3, EnumSet.allOf(Ontology.RelationType.class));
         Set<AnatEntity> expAncestors = new HashSet<>(Arrays.asList(ae1, ae2, ae2p));

bgee-webapp/src/test/java/org/bgee/controller/CommandParentTest.java
@@ -9,7 +9,6 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.HashSet;
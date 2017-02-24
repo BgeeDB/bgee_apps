983965dfb4d2bee572d957cd13e20875006db17d
fbastian <frederic.bastian@gmail.com>
2017-02-24 01:55:17 +0100

    improve and reformat RawExpressionCallDAO

--- bgee-dao-api/src/main/java/org/bgee/model/dao/api/expressiondata/RawExpressionCallDAO.java
+++ bgee-dao-api/src/main/java/org/bgee/model/dao/api/expressiondata/RawExpressionCallDAO.java
@@ -5,33 +5,47 @@ import java.util.Collection;
 
 import org.bgee.model.dao.api.DAO;
 import org.bgee.model.dao.api.DAOResultSet;
-import org.bgee.model.dao.api.TransferObject;
+import org.bgee.model.dao.api.EntityTO;
 import org.bgee.model.dao.api.exception.DAOException;
 
 /**
  * DAO defining queries using or retrieving {@link RawExpressionCallTO}s. 
  * 
  * @author  Valentine Rech de Laval
+ * @author  Frederic Bastian
  * @version Bgee 14, Feb. 2017
  * @since   Bgee 14, Feb. 2017
  * @see RawExpressionCallTO
  */
 public interface RawExpressionCallDAO extends DAO<RawExpressionCallDAO.Attribute> {
-
+    
     /**
      * {@code Enum} used to define the attributes to populate in the {@code RawExpressionCallTO}s 
      * obtained from this {@code RawExpressionCallDAO}.
      * <ul>
-     * <li>{@code ID}: corresponds to {@link RawExpressionCallDTO#getId()}.
-     * <li>{@code GENE_ID}: corresponds to {@link RawExpressionCallDTO#getSourceId()}.
-     * <li>{@code CONDITION_ID}: corresponds to {@link RawExpressionCallDTO#getTargetId()}.
+     * <li>{@code ID}: corresponds to {@link RawExpressionCallTO#getId()}.
+     * <li>{@code BGEE_GENE_ID}: corresponds to {@link RawExpressionCallTO#getBgeeGeneId()}.
+     * <li>{@code CONDITION_ID}: corresponds to {@link RawExpressionCallTO#getConditionId()}.
+     * <li>{@code AFFYMETRIX_MEAN_RANK}: corresponds to {@link RawExpressionCallTO#getAffymetrixMeanRank()}.
+     * <li>{@code RNA_SEQ_MEAN_RANK}: corresponds to {@link RawExpressionCallTO#getRNASeqMeanRank()}.
+     * <li>{@code EST_RANK}: corresponds to {@link RawExpressionCallTO#getESTRank()}.
+     * <li>{@code IN_SITU_RANK}: corresponds to {@link RawExpressionCallTO#getInSituRank()}.
+     * <li>{@code AFFYMETRIX_MEAN_RANK_NORM}: corresponds to {@link RawExpressionCallTO#getAffymetrixMeanRankNorm()}.
+     * <li>{@code RNA_SEQ_MEAN_RANK_NORM}: corresponds to {@link RawExpressionCallTO#getRNASeqMeanRankNorm()}.
+     * <li>{@code EST_RANK_NORM}: corresponds to {@link RawExpressionCallTO#getESTRankNorm()}.
+     * <li>{@code IN_SITU_RANK_NORM}: corresponds to {@link RawExpressionCallTO#getInSituRankNorm()}.
+     * <li>{@code AFFYMETRIX_DISTINCT_RANK_SUM}: corresponds to {@link RawExpressionCallTO#getAffymetrixDistinctRankSum()}.
+     * <li>{@code RNA_SEQ_DISTINCT_RANK_SUM}: corresponds to {@link RawExpressionCallTO#getRNASeqDistinctRankSum()}.
      * </ul>
-     * @see org.bgee.model.dao.api.DAO#setAttributes(Collection)
-     * @see org.bgee.model.dao.api.DAO#setAttributes(Enum[])
-     * @see org.bgee.model.dao.api.DAO#clearAttributes()
      */
     public enum Attribute implements DAO.Attribute {
-        ID("id"), GENE_ID("geneId"), CONDITION_ID("conditionId");
+        ID("id"), BGEE_GENE_ID("bgeeGeneId"), CONDITION_ID("conditionId"), 
+        AFFYMETRIX_MEAN_RANK("affymetrixMeanRank"), RNA_SEQ_MEAN_RANK("rnaSeqMeanRank"), 
+        EST_RANK("estRank"), IN_SITU_RANK("inSituRank"), 
+        AFFYMETRIX_MEAN_RANK_NORM("affymetrixMeanRankNorm"), RNA_SEQ_MEAN_RANK_NORM("rnaSeqMeanRankNorm"), 
+        EST_RANK_NORM("estRankNorm"), IN_SITU_RANK_NORM("inSituRankNorm"), 
+        AFFYMETRIX_DISTINCT_RANK_SUM("affymetrixDistinctRankSum"), 
+        RNA_SEQ_DISTINCT_RANK_SUM("rnaSeqDistinctRankSum");
         
         /**
          * A {@code String} that is the corresponding field name in {@code RelationTO} class.
@@ -42,13 +56,12 @@ public interface RawExpressionCallDAO extends DAO<RawExpressionCallDAO.Attribute
         private Attribute(String fieldName) {
             this.fieldName = fieldName;
         }
-
         @Override
         public String getTOFieldName() {
             return this.fieldName;
         }
     }
-
+    
     /** 
      * TODO javadoc
      * @param speciesId
@@ -57,163 +70,122 @@ public interface RawExpressionCallDAO extends DAO<RawExpressionCallDAO.Attribute
      * @throws DAOException
      */
     public RawExpressionCallTOResultSet getExpressionCallsOrderedByGeneIdAndExprId(int speciesId,
-        Collection<ConditionDAO.Attribute> condParameters) throws DAOException;
-
-  /**
-   * {@code DAOResultSet} specifics to {@code RawExpressionCallTO}s
-   * 
-   * @author  Valentine Rech de Laval
-   * @version Bgee 14, Feb. 2017
-   * @since   Bgee 14, Feb. 2017
-   */
-  public interface RawExpressionCallTOResultSet extends DAOResultSet<RawExpressionCallTO> {
-  }
-  
-  /**
-   * {@code EntityTO} representing a raw expression call in the Bgee database.
-   * 
-   * @author  Valentine Rech de Laval
-   * @version Bgee 14, Feb. 2017
-   * @since   Bgee 14, Feb. 2017
-   */
-  public class RawExpressionCallTO extends TransferObject {
-
-      private static final long serialVersionUID = -1057540315343857464L;
-      
-      /**
-       * A {@code String} representing the ID of this call.
-       */
-      private Integer id;
-      /**
-       * A {@code String} representing the ID of the gene associated to this call.
-       */
-      private Integer bgeeGeneId;
-      /**
-       * A {@code String} representing the ID of the condition associated this call.
-       */
-      private Integer conditionId;
-
-      private BigDecimal rnaSeqMeanRank;
-      private BigDecimal affymetrixMeanRank;
-
-      private BigDecimal estRank;
-      private BigDecimal inSituRank;
-
-      private BigDecimal affymetrixMaxRank;
-      private BigDecimal rnaSeqMaxRank;
-      private BigDecimal estMaxRank;
-      private BigDecimal inSituMaxRank;
-
-      private BigDecimal affymetrixMeanRankNorm;
-      private BigDecimal rnaSeqMeanRankNorm;
-
-      private BigDecimal estRankNorm;
-      private BigDecimal inSituRankNorm;
-
-      private BigDecimal affymetrixDistinctRankSum;
-      private BigDecimal rnaSeqDistinctRankSum;
-
-      public RawExpressionCallTO(Integer id, Integer bgeeGeneId, Integer conditionId,
-          BigDecimal rnaSeqMeanRank, BigDecimal affymetrixMeanRank, BigDecimal estRank,
-          BigDecimal inSituRank, BigDecimal affymetrixMaxRank, BigDecimal rnaSeqMaxRank,
-          BigDecimal estMaxRank, BigDecimal inSituMaxRank, BigDecimal affymetrixMeanRankNorm,
-          BigDecimal rnaSeqMeanRankNorm, BigDecimal estRankNorm, BigDecimal inSituRankNorm,
-          BigDecimal affymetrixDistinctRankSum, BigDecimal rnaSeqDistinctRankSum) {
-          super();
-          this.id = id;
-          this.bgeeGeneId = bgeeGeneId;
-          this.conditionId = conditionId;
-          this.rnaSeqMeanRank = rnaSeqMeanRank;
-          this.affymetrixMeanRank = affymetrixMeanRank;
-          this.estRank = estRank;
-          this.inSituRank = inSituRank;
-          this.affymetrixMaxRank = affymetrixMaxRank;
-          this.rnaSeqMaxRank = rnaSeqMaxRank;
-          this.estMaxRank = estMaxRank;
-          this.inSituMaxRank = inSituMaxRank;
-          this.affymetrixMeanRankNorm = affymetrixMeanRankNorm;
-          this.rnaSeqMeanRankNorm = rnaSeqMeanRankNorm;
-          this.estRankNorm = estRankNorm;
-          this.inSituRankNorm = inSituRankNorm;
-          this.affymetrixDistinctRankSum = affymetrixDistinctRankSum;
-          this.rnaSeqDistinctRankSum = rnaSeqDistinctRankSum;
-      }
-      
-      public Integer getId() {
-          return this.id;
-      }
-      public Integer getBgeeGeneId() {
-          return this.bgeeGeneId;
-      }
-      public Integer getConditionId() {
-          return this.conditionId;
-      }
-
-    public BigDecimal getRnaSeqMeanRank() {
-        return rnaSeqMeanRank;
+            Collection<ConditionDAO.Attribute> condParameters) throws DAOException;
+    
+    /**
+     * {@code DAOResultSet} specifics to {@code RawExpressionCallTO}s
+     * 
+     * @author  Valentine Rech de Laval
+     * @version Bgee 14, Feb. 2017
+     * @since   Bgee 14, Feb. 2017
+     */
+    public interface RawExpressionCallTOResultSet extends DAOResultSet<RawExpressionCallTO> {
     }
-
-    public BigDecimal getAffymetrixMeanRank() {
-        return affymetrixMeanRank;
+    
+    /**
+     * {@code EntityTO} representing a raw expression call in the Bgee database.
+     * 
+     * @author  Valentine Rech de Laval
+     * @author  Frederic Bastian
+     * @version Bgee 14, Feb. 2017
+     * @since   Bgee 14, Feb. 2017
+     */
+    public class RawExpressionCallTO extends EntityTO<Integer> {
+        
+        private static final long serialVersionUID = -1057540315343857464L;
+        
+        /**
+         * An {@code Integer} representing the ID of the gene associated to this call.
+         */
+        private final Integer bgeeGeneId;
+        /**
+         * An {@code Integer} representing the ID of the condition associated to this call.
+         */
+        private final Integer conditionId;
+        
+        private final BigDecimal rnaSeqMeanRank;
+        private final BigDecimal affymetrixMeanRank;
+        private final BigDecimal estRank;
+        private final BigDecimal inSituRank;
+        
+        private final BigDecimal affymetrixMeanRankNorm;
+        private final BigDecimal rnaSeqMeanRankNorm;
+        private final BigDecimal estRankNorm;
+        private final BigDecimal inSituRankNorm;
+        
+        private final BigDecimal affymetrixDistinctRankSum;
+        private final BigDecimal rnaSeqDistinctRankSum;
+        
+        public RawExpressionCallTO(Integer id, Integer bgeeGeneId, Integer conditionId,
+                BigDecimal affymetrixMeanRank, BigDecimal rnaSeqMeanRank, BigDecimal estRank,
+                BigDecimal inSituRank, BigDecimal affymetrixMeanRankNorm,
+                BigDecimal rnaSeqMeanRankNorm, BigDecimal estRankNorm, BigDecimal inSituRankNorm,
+                BigDecimal affymetrixDistinctRankSum, BigDecimal rnaSeqDistinctRankSum) {
+            super(id);
+            this.bgeeGeneId = bgeeGeneId;
+            this.conditionId = conditionId;
+            this.affymetrixMeanRank = affymetrixMeanRank;
+            this.rnaSeqMeanRank = rnaSeqMeanRank;
+            this.estRank = estRank;
+            this.inSituRank = inSituRank;
+            this.affymetrixMeanRankNorm = affymetrixMeanRankNorm;
+            this.rnaSeqMeanRankNorm = rnaSeqMeanRankNorm;
+            this.estRankNorm = estRankNorm;
+            this.inSituRankNorm = inSituRankNorm;
+            this.affymetrixDistinctRankSum = affymetrixDistinctRankSum;
+            this.rnaSeqDistinctRankSum = rnaSeqDistinctRankSum;
+        }
+        
+        public Integer getBgeeGeneId() {
+            return this.bgeeGeneId;
+        }
+        public Integer getConditionId() {
+            return this.conditionId;
+        }
+        
+        public BigDecimal getRNASeqMeanRank() {
+            return rnaSeqMeanRank;
+        }
+        public BigDecimal getAffymetrixMeanRank() {
+            return affymetrixMeanRank;
+        }
+        public BigDecimal getESTRank() {
+            return estRank;
+        }
+        public BigDecimal getInSituRank() {
+            return inSituRank;
+        }
+        
+        public BigDecimal getAffymetrixMeanRankNorm() {
+            return affymetrixMeanRankNorm;
+        }
+        public BigDecimal getRNASeqMeanRankNorm() {
+            return rnaSeqMeanRankNorm;
+        }
+        public BigDecimal getESTRankNorm() {
+            return estRankNorm;
+        }
+        public BigDecimal getInSituRankNorm() {
+            return inSituRankNorm;
+        }
+        
+        public BigDecimal getAffymetrixDistinctRankSum() {
+            return affymetrixDistinctRankSum;
+        }
+        public BigDecimal getRNASeqDistinctRankSum() {
+            return rnaSeqDistinctRankSum;
+        }
+        
+        @Override
+        public String toString() {
+            return "RawExpressionCallTO [id=" + this.getId() + ", bgeeGeneId=" + bgeeGeneId 
+                    + ", conditionId=" + conditionId + ", rnaSeqMeanRank=" + rnaSeqMeanRank 
+                    + ", affymetrixMeanRank=" + affymetrixMeanRank + ", estRank=" + estRank 
+                    + ", inSituRank=" + inSituRank + ", affymetrixMeanRankNorm=" 
+                    + affymetrixMeanRankNorm + ", rnaSeqMeanRankNorm=" + rnaSeqMeanRankNorm
+                    + ", estRankNorm=" + estRankNorm + ", inSituRankNorm=" + inSituRankNorm 
+                    + ", affymetrixDistinctRankSum=" + affymetrixDistinctRankSum 
+                    + ", rnaSeqDistinctRankSum=" + rnaSeqDistinctRankSum + "]";
+        }
     }
-
-    public BigDecimal getEstRank() {
-        return estRank;
-    }
-
-    public BigDecimal getInSituRank() {
-        return inSituRank;
-    }
-
-    public BigDecimal getAffymetrixMaxRank() {
-        return affymetrixMaxRank;
-    }
-
-    public BigDecimal getRnaSeqMaxRank() {
-        return rnaSeqMaxRank;
-    }
-
-    public BigDecimal getEstMaxRank() {
-        return estMaxRank;
-    }
-
-    public BigDecimal getInSituMaxRank() {
-        return inSituMaxRank;
-    }
-
-    public BigDecimal getAffymetrixMeanRankNorm() {
-        return affymetrixMeanRankNorm;
-    }
-
-    public BigDecimal getRnaSeqMeanRankNorm() {
-        return rnaSeqMeanRankNorm;
-    }
-
-    public BigDecimal getEstRankNorm() {
-        return estRankNorm;
-    }
-
-    public BigDecimal getInSituRankNorm() {
-        return inSituRankNorm;
-    }
-
-    public BigDecimal getAffymetrixDistinctRankSum() {
-        return affymetrixDistinctRankSum;
-    }
-
-    public BigDecimal getRnaSeqDistinctRankSum() {
-        return rnaSeqDistinctRankSum;
-    }
-
-    @Override
-    public String toString() {
-        return "RawExpressionCallTO [id=" + id + ", bgeeGeneId=" + bgeeGeneId + ", conditionId="
-            + conditionId + ", rnaSeqMeanRank=" + rnaSeqMeanRank + ", affymetrixMeanRank=" + affymetrixMeanRank
-            + ", estRank=" + estRank + ", inSituRank=" + inSituRank + ", affymetrixMaxRank=" + affymetrixMaxRank
-            + ", rnaSeqMaxRank=" + rnaSeqMaxRank + ", estMaxRank=" + estMaxRank + ", inSituMaxRank=" + inSituMaxRank
-            + ", affymetrixMeanRankNorm=" + affymetrixMeanRankNorm + ", rnaSeqMeanRankNorm=" + rnaSeqMeanRankNorm
-            + ", estRankNorm=" + estRankNorm + ", inSituRankNorm=" + inSituRankNorm + ", affymetrixDistinctRankSum="
-            + affymetrixDistinctRankSum + ", rnaSeqDistinctRankSum=" + rnaSeqDistinctRankSum + "]";
-    }
-  }
 }

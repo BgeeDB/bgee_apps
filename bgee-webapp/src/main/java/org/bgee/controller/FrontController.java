d6fd5c9811259ae266c7df19e8c60c6baf3c014b
jwollbrett <julien.wollbrett@unil.ch>
2017-05-05 17:59:37 +0200

    add RPackageCommand to FrontController

--- bgee-webapp/src/main/java/org/bgee/controller/FrontController.java
+++ bgee-webapp/src/main/java/org/bgee/controller/FrontController.java
@@ -245,6 +245,9 @@ public class FrontController extends HttpServlet {
             } else if (requestParameters.isADAOPageCategory()) {
                 controller = new CommandDAO(response, requestParameters, this.prop, factory, 
                         serviceFactory, this.jobService, user);
+            }else if (requestParameters.isARPackagePageCategory()) {
+                controller = new CommandRPackage(response, requestParameters, this.prop, factory, 
+                        serviceFactory, this.jobService, user);
             } else if (requestParameters.isAStatsPageCategory()) {
                 //no specific controllers for this for now. 
                 //We simply respond with a 'success no content' so that the client get no errors, 

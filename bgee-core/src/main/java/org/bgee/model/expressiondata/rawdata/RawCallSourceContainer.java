9ad70ea30f3014c59903bb2df596b45aec16375a
Julien Wollbrett <jwollbrett@bio-51-170.unil.ch>
2018-09-11 15:51:51 +0200

    continue design of raw data in bgee-core

--- bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/RawCallSourceContainer.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/RawCallSourceContainer.java
@@ -9,16 +9,13 @@ import org.bgee.model.expressiondata.rawdata.microarray.AffymetrixProbeset;
 public class RawCallSourceContainer {
     private final static Logger log = LogManager.getLogger(RawCallSourceContainer.class.getName());
 
-    private final Stream<AffymetrixProbeset> affymetrixProbesets;
-
     /**
      * @return  A {@code Stream} of {@code AffymetrixProbeset}s.
-     *          If the returned value is {@code null}, it means that this data type was not requested.
-     *          If the {@code Stream} is not {@code null} but contains no element,
+     *          If the {@code Stream} contains no element,
      *          it means that there were no data of this type for the requested parameters.
      */
-    public Stream<AffymetrixProbeset> getAffymetrixProbesets() {
-        return this.affymetrixProbesets;
+    public Stream<AffymetrixProbeset> loadAffymetrixProbesets() {
+        return null;
     }
 
 }
---
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/RawDataCondition.java
@@ -0,0 +1,5 @@
+package org.bgee.model.expressiondata.rawdata;
+
+public class RawDataCondition {
+
+}

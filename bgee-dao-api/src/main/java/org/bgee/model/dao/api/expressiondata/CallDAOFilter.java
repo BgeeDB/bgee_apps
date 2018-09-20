1d39efc53f7ef400bda6fecb6c13e10e8557bcc9
fbastian <frederic.bastian@gmail.com>
2018-09-20 15:42:45 +0200

    redesign DAO data filters based on design in bgee-core

--- bgee-dao-api/src/main/java/org/bgee/model/dao/api/expressiondata/CallDAOFilter.java
+++ bgee-dao-api/src/main/java/org/bgee/model/dao/api/expressiondata/CallDAOFilter.java
@@ -1,10 +1,7 @@
 package org.bgee.model.dao.api.expressiondata;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.Set;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -17,21 +14,9 @@ import org.apache.logging.log4j.Logger;
  * @version Bgee 14, Jun. 2018
  * @since   Bgee 13, Oct. 2015
  */
-public class CallDAOFilter {
+public class CallDAOFilter extends DAODataFilter<DAOConditionFilter> {
     private final static Logger log = LogManager.getLogger(CallDAOFilter.class.getName());
 
-    /**
-     * @see #getGeneIds()
-     */
-    private final Set<Integer> geneIds;
-    /**
-     * @see #getSpeciesIds()
-     */
-    private final Set<Integer> speciesIds;
-    /**
-     * @see #getConditionFilters()
-     */
-    private final LinkedHashSet<DAOConditionFilter> conditionFilters;
     /**
      * @see #getDataFilters()
      */
@@ -63,27 +48,13 @@ public class CallDAOFilter {
     public CallDAOFilter(Collection<Integer> geneIds, Collection<Integer> speciesIds, 
             Collection<DAOConditionFilter> conditionFilters, Collection<CallDataDAOFilter> dataFilters)
                     throws IllegalArgumentException {
+        super(geneIds, speciesIds, conditionFilters);
         log.entry(geneIds, speciesIds, conditionFilters, dataFilters);
-        if (geneIds != null && geneIds.stream().anyMatch(id -> id == null)) {
-            throw log.throwing(new IllegalArgumentException("No gene ID can be null"));
-        }
-        if (speciesIds != null && speciesIds.stream().anyMatch(id -> id == null)) {
-            throw log.throwing(new IllegalArgumentException("No species ID can be null"));
-        }
-        if (conditionFilters != null && conditionFilters.stream().anyMatch(cf -> cf == null)) {
-            throw log.throwing(new IllegalArgumentException("No DAOConditionFilter can be null"));
-        }
+
         if (dataFilters != null && dataFilters.stream().anyMatch(df -> df == null)) {
             throw log.throwing(new IllegalArgumentException("No CallDataDAOFilter can be null"));
         }
 
-        this.geneIds = Collections.unmodifiableSet(
-                geneIds == null? new HashSet<>(): new HashSet<>(geneIds));
-        this.speciesIds = Collections.unmodifiableSet(
-                speciesIds == null? new HashSet<>(): new HashSet<>(speciesIds));
-        //we'll use defensive copying for this one, no unmodifiableLinkedHashSet method
-        this.conditionFilters = conditionFilters == null? new LinkedHashSet<>(): 
-            new LinkedHashSet<>(conditionFilters);
         this.dataFilters = dataFilters == null? new LinkedHashSet<>(): 
             new LinkedHashSet<>(dataFilters);
         
@@ -95,31 +66,6 @@ public class CallDAOFilter {
         log.exit();
     }
 
-    /**
-     * @return  An unmodifiable {@code Set} of {@code Integer}s containing the IDs of genes used
-     *          to filter expression queries. Can be {@code null} or empty.
-     */
-    public Set<Integer> getGeneIds() {
-        return geneIds;
-    }
-    /**
-     * @return  An unmodifiable {@code Set} of {@code Integer}s containing the IDs of species used
-     *          to filter expression queries. Can be {@code null} or empty.
-     */
-    public Set<Integer> getSpeciesIds() {
-        return speciesIds;
-    }
-    /**
-     * @return  A {@code LinkedHashSet} of {@code DAOConditionFilter}s to configure the filtering 
-     *          of conditions with expression data. If several {@code ConditionFilter}s are provided, 
-     *          they are seen as "OR" conditions. Can be {@code null} or empty. 
-     *          Provided as a {@code LinkedHashSet} for convenience, to consistently set parameters 
-     *          in queries.
-     */
-    public LinkedHashSet<DAOConditionFilter> getConditionFilters() {
-        //defensive copying, no unmodifiable LinkedHashSet
-        return new LinkedHashSet<>(conditionFilters);
-    }
     /**
      * @return  A {@code LinkedHashSet} of {@code CallDataDAOFilter}s to configure the filtering
      *          of conditions with expression data. If several {@code CallDataDAOFilter}s are provided,
@@ -133,14 +79,12 @@ public class CallDAOFilter {
         return new LinkedHashSet<>(dataFilters);
     }
 
+
     @Override
     public int hashCode() {
         final int prime = 31;
-        int result = 1;
-        result = prime * result + ((conditionFilters == null) ? 0 : conditionFilters.hashCode());
+        int result = super.hashCode();
         result = prime * result + ((dataFilters == null) ? 0 : dataFilters.hashCode());
-        result = prime * result + ((geneIds == null) ? 0 : geneIds.hashCode());
-        result = prime * result + ((speciesIds == null) ? 0 : speciesIds.hashCode());
         return result;
     }
     @Override
@@ -148,39 +92,18 @@ public class CallDAOFilter {
         if (this == obj) {
             return true;
         }
-        if (obj == null) {
+        if (!super.equals(obj)) {
             return false;
         }
-        if (getClass() != obj.getClass()) {
+        if (!(obj instanceof CallDAOFilter)) {
             return false;
         }
         CallDAOFilter other = (CallDAOFilter) obj;
-        if (conditionFilters == null) {
-            if (other.conditionFilters != null) {
-                return false;
-            }
-        } else if (!conditionFilters.equals(other.conditionFilters)) {
-            return false;
-        }
         if (dataFilters == null) {
             if (other.dataFilters != null) {
                 return false;
             }
         } else if (!dataFilters.equals(other.dataFilters)) {
-            return false;
-        }
-        if (geneIds == null) {
-            if (other.geneIds != null) {
-                return false;
-            }
-        } else if (!geneIds.equals(other.geneIds)) {
-            return false;
-        }
-        if (speciesIds == null) {
-            if (other.speciesIds != null) {
-                return false;
-            }
-        } else if (!speciesIds.equals(other.speciesIds)) {
             return false;
         }
         return true;
@@ -189,9 +112,9 @@ public class CallDAOFilter {
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        builder.append("CallDAOFilter [geneIds=").append(geneIds)
-               .append(", speciesIds=").append(speciesIds)
-               .append(", conditionFilters=").append(conditionFilters)
+        builder.append("CallDAOFilter [geneIds=").append(getGeneIds())
+               .append(", speciesIds=").append(getSpeciesIds())
+               .append(", conditionFilters=").append(getConditionFilters())
                .append(", dataFilters=").append(dataFilters)
                .append("]");
         return builder.toString();
---
+++ bgee-dao-api/src/main/java/org/bgee/model/dao/api/expressiondata/rawdata/DAORawDataFilter.java
@@ -0,0 +1,32 @@
+package org.bgee.model.dao.api.expressiondata.rawdata;
+
+import java.util.Collection;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.dao.api.expressiondata.DAODataFilter;
+
+public class DAORawDataFilter extends DAODataFilter<DAORawDataConditionFilter> {
+    private final static Logger log = LogManager.getLogger(DAORawDataFilter.class.getName());
+
+    public DAORawDataFilter(Collection<Integer> geneIds, Collection<Integer> speciesIds,
+            Collection<DAORawDataConditionFilter> conditionFilters) {
+        super(geneIds, speciesIds, conditionFilters);
+        
+        if (this.getGeneIds().isEmpty() && this.getSpeciesIds().isEmpty() &&
+                this.getConditionFilters().isEmpty()) {
+            throw log.throwing(new IllegalArgumentException("No filters provided"));
+        }
+    }
+
+    //since we have no attributes but the one in DAODataFilter, we do not implement equals/hashCode for now.
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("DAORawDataFilter [geneIds()=").append(getGeneIds())
+               .append(", speciesIds()=").append(getSpeciesIds())
+               .append(", conditionFilters()=").append(getConditionFilters()).append("]");
+        return builder.toString();
+    }
+}

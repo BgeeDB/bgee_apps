f4f6b7a69cd3688730101a2d4472d1c8ef541b3a
fbastian <frederic.bastian@gmail.com>
2019-03-04 20:07:48 +0100

    Start implementing AnatEntitySimilarityService, WIP

--- bgee-core/src/main/java/org/bgee/model/anatdev/multispemapping/AnatEntitySimilarityService.java
+++ bgee-core/src/main/java/org/bgee/model/anatdev/multispemapping/AnatEntitySimilarityService.java
@@ -1,10 +1,11 @@
 package org.bgee.model.anatdev.multispemapping;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,10 +14,19 @@ import org.apache.logging.log4j.Logger;
 import org.bgee.model.Service;
 import org.bgee.model.ServiceFactory;
 import org.bgee.model.anatdev.AnatEntity;
+import org.bgee.model.dao.api.anatdev.mapping.SummarySimilarityAnnotationDAO;
 import org.bgee.model.dao.api.anatdev.mapping.SummarySimilarityAnnotationDAO.SimAnnotToAnatEntityTO;
 import org.bgee.model.dao.api.anatdev.mapping.SummarySimilarityAnnotationDAO.SummarySimilarityAnnotationTO;
+import org.bgee.model.ontology.Ontology;
 import org.bgee.model.species.Taxon;
 
+/**
+ * A {@code Service} for {@code AnatEntitySimilarity}.
+ *
+ * @author Frederic Bastian
+ * @version Bgee 14 Mar. 2019
+ * @since Bgee 14 Mar. 2019
+ */
 public class AnatEntitySimilarityService extends Service {
     private final static Logger log = LogManager.getLogger(AnatEntitySimilarityService.class.getName());
 
@@ -30,37 +40,58 @@ public class AnatEntitySimilarityService extends Service {
     }
 
     /**
-     * Load anatomical entity similarities from provided {@code taxonId}, {@code speciesIds},
-     * and {@code onlyTrusted}.
+     * Load positive anatomical entity similarities valid for the requested {@code taxonId}.
      * 
      * @param taxonId       An {@code Integer} that is the NCBI ID of the taxon for which the similarity 
-     *                      annotations should be valid, including all its ancestral taxa.
-     * @param speciesIds    A {@code Set} of {@code Integer}s that are IDs of the species
-     *                      for which the similarity annotations should be restricted.
-     *                      If empty or {@code null} all available species are used.
+     *                      annotations should be valid for.
      * @param onlyTrusted   A {@code boolean} defining whether results should be restricted 
-     *                      to "trusted" annotations.
-     * @return              The {@code Stream} of {@link AnatEntitySimilarity} that are anat. entity 
-     *                      similarities from provided {@code taxonId}, {@code speciesIds},
-     *                      and {@code onlyTrusted}.
+     *                      to "trusted" annotations. If {@code true}, only trusted annotations are returned.
+     * @return              The {@code Stream} of {@link AnatEntitySimilarity}s.
      */
-    public Stream<AnatEntitySimilarity> loadAnatEntitySimilarities(Integer taxonId,
-            Set<Integer> speciesIds, boolean onlyTrusted) {
-        log.entry(taxonId, speciesIds, onlyTrusted);
-        //FIXME: update method
-        final Map<Integer, SummarySimilarityAnnotationTO> simAnnotations = Collections.unmodifiableMap(
-                this.getDaoManager().getSummarySimilarityAnnotationDAO()
-                .getSummarySimilarityAnnotations(taxonId, true, false, true, onlyTrusted, null).stream()
-                .filter(to -> taxonId.equals(to.getTaxonId()))
-                .collect(Collectors.toMap(SummarySimilarityAnnotationTO::getId, Function.identity())));
+    public Stream<AnatEntitySimilarity> loadPositiveAnatEntitySimilarities(int taxonId, boolean onlyTrusted) {
+        log.entry(taxonId, onlyTrusted);
+        if (taxonId <= 0) {
+            throw log.throwing(new IllegalArgumentException("Taxon ID must be stricly positive."));
+        }
+
+        //We need the taxon ontology for the requested taxon and its ancestors and descendants,
+        //in order to correctly filter the similarity annotations to return.
+        Ontology<Taxon, Integer> taxonOnt = this.getServiceFactory().getOntologyService()
+                .getTaxonOntologyFromTaxonIds(Collections.singleton(taxonId), false, true, true);
+        if (taxonOnt.getElement(taxonId) == null) {
+            throw log.throwing(new IllegalArgumentException("Taxon ID not found."));
+        }
+
+        //Now, we retrieve all similarity annotations annotated to the requested taxon,
+        //but also to its ancestors and descendants, to then filter them appropriately
+        SummarySimilarityAnnotationDAO simAnnotDAO = this.getDaoManager().getSummarySimilarityAnnotationDAO();
+        Map<Integer, SummarySimilarityAnnotationTO> idToAnnots = simAnnotDAO
+                .getSummarySimilarityAnnotations(taxonId, true, true, true, onlyTrusted, null)
+                .stream().collect(Collectors.toMap(a -> a.getId(), a -> a));
+        Map<Integer, Set<String>> idToAnatEntities = simAnnotDAO
+                .getSimAnnotToAnatEntity(taxonId, true, true, true, onlyTrusted)
+                .stream().collect(Collectors.toMap(
+                        simToAnat -> simToAnat.getSummarySimilarityAnnotationId(),
+                        simToAnat -> new HashSet<>(Arrays.asList(simToAnat.getAnatEntityId())),
+                        (v1, v2) -> {v1.addAll(v2); return v1;}));
+
+        //Now, perform filtering. The aim is to identify when to keep a multiple-entity annotations
+        //and discard the corresponding single-entity annotations, or the other way around.
+        //Multiple-entity annotations are valid when:
+        //* all the entities part of the annotation exist in the requested taxon or in some sub-taxa
+        //(meaning, each entity has a corresponding single-entity annotation for the requested taxon,
+        //or ancestors or descendants of the requested taxon)
+        //* but the entities do not all exist in all the sub-taxa of the requested taxon (meaning, some entity
+        //are annotated to different sub-taxa of the requested taxon, including the requested taxon itself).
+        Set<SummarySimilarityAnnotationTO> toDiscard = new HashSet<>();
 
         //Load the taxa we're gonna need in a Map where the key is the taxon ID
-        Set<Integer> allTaxonIds = simAnnotations.values().stream()
-                .map(annot -> annot.getTaxonId())
-                .collect(Collectors.toSet());
-        final Map<Integer, Taxon> taxa = Collections.unmodifiableMap(
-                this.getServiceFactory().getTaxonService().loadTaxaByIds(allTaxonIds)
-                .collect(Collectors.toMap(t -> t.getId(), t -> t)));
+//        Set<Integer> allTaxonIds = simAnnotations.values().stream()
+//                .map(annot -> annot.getTaxonId())
+//                .collect(Collectors.toSet());
+//        final Map<Integer, Taxon> taxa = Collections.unmodifiableMap(
+//                this.getServiceFactory().getTaxonService().loadTaxaByIds(allTaxonIds)
+//                .collect(Collectors.toMap(t -> t.getId(), t -> t)));
 
         //Now we need to get all mappings between anat. entities, to be able to load the anat. entities we need
         Map<Integer, List<SimAnnotToAnatEntityTO>> groupedMappingTOs = 
@@ -78,7 +109,7 @@ public class AnatEntitySimilarityService extends Service {
 
         //Now we produce the AnatEntitySimilarities
         return log.exit(groupedMappingTOs.entrySet().stream().map(e -> {
-            SummarySimilarityAnnotationTO annot = simAnnotations.get(e.getKey());
+            SummarySimilarityAnnotationTO annot = idToAnnots.get(e.getKey());
             if (annot == null) {
                 throw log.throwing(new IllegalStateException(
                         "Missing annotation with ID " + e.getKey()));
@@ -93,7 +124,7 @@ public class AnatEntitySimilarityService extends Service {
                         return anatEntity;
                     })
                     .collect(Collectors.toSet());
-            Taxon taxon = taxa.get(annot.getTaxonId());
+            Taxon taxon = taxonOnt.getElement(annot.getTaxonId());
             if (taxon == null) {
                 throw log.throwing(new IllegalStateException(
                         "Missing taxon with ID " + annot.getTaxonId()));

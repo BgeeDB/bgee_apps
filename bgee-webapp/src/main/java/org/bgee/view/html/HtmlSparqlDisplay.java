9b68c952a3e316188291e6213f9c9a7c0b2593cc
Julien Wollbrett <jwollbrett@bio-51-170.unil.ch>
2019-05-13 14:49:49 +0200

    Update SPARQL endpoint page

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlSparqlDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlSparqlDisplay.java
@@ -39,8 +39,23 @@ public class HtmlSparqlDisplay extends HtmlParentDisplay implements SparqlDispla
         super(response, requestParameters, prop, factory);
     }
     
+    private static String SPARQL_QUERY_EXAMPLE = "http://biosoda.expasy.org:8080/rdf4j-server/"
+            + "repositories/bgeelight?query=PREFIX%20orth%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fnet%2"
+            + "Forth%23%3E%0APREFIX%20up%3A%20%3Chttp%3A%2F%2Fpurl.uniprot.org%2Fcore%2F%3E%0AP"
+            + "REFIX%20genex%3A%20%3Chttp%3A%2F%2Fpurl.org%2Fgenex%23%3E%0APREFIX%20obo%3A%20%3"
+            + "Chttp%3A%2F%2Fpurl.obolibrary.org%2Fobo%2F%3E%0ASELECT%20DISTINCT%20%3FanatEntit"
+            + "y%20%3FanatName%20%7B%0A%09%3Fseq%20a%20orth%3AGene%20.%0A%09%3Fexpr%20genex%3Ah"
+            + "asSequenceUnit%20%3Fseq%20.%0A%09%3Fseq%20rdfs%3Alabel%20%3FgeneName%20.%0A%09%3"
+            + "Fexpr%20genex%3AhasExpressionCondition%20%3Fcond%20.%0A%09%3Fcond%20genex%3AhasA"
+            + "natomicalEntity%20%3FanatEntity%20.%0A%09%3FanatEntity%20rdfs%3Alabel%20%3FanatN"
+            + "ame%20.%0A%09%3Fcond%20obo%3ARO_0002162%20%3Chttp%3A%2F%2Fpurl.uniprot.org%2Ftax"
+            + "onomy%2F9606%3E%20.%20%0A%09FILTER%20(LCASE(%3FgeneName)%20%3D%20LCASE(%27apoc1%"
+            + "27)%20)%0A%7D&format=JSON&limit=100&offset=0&inference=false";
+    
     @Override
     public void displaySparql() {
+        
+        
         log.entry();
 
         this.startDisplay("Bgee SPARQL endpoint");
@@ -48,21 +63,26 @@ public class HtmlSparqlDisplay extends HtmlParentDisplay implements SparqlDispla
         this.writeln("<div class='row'>");
         this.writeln("<div class='" + CENTERED_ELEMENT_CLASS + "'>");
 
-        this.writeln("<h1>About Bgee SPARQL endpoint</h1>");
+        this.writeln("<h1>Bgee SPARQL endpoint</h1>");
 
-        this.writeln("<p>The Bgee SPARQL endpoint is reachable by using your preferable programming "
-                + "language or SPARQL interface through the URL address below : </p>");
-        this.writeln("<p><center> "
-                + "<a href = 'http://biosoda.expasy.org:8080/rdf4j-server/repositories/bgeelight'"
-                + "title = 'Link to the Bgee SPARQL endpoint' class='external_link' "
-                + "target = '_blank'> "
-                + "http://biosoda.expasy.org:8080/rdf4j-server/repositories/bgeelight'</a></center></p>");
-        this.writeln("<p>It was created using an Ontology Based Data "
+        this.writeln("<p>The Bgee SPARQL endpoint was created using an Ontology Based Data "
                 + "Access (OBDA) approach allowing to create a virtual RDF serialisation without exporting "
                 + "data from the original relational database. It queries data stored in the "
                 + "<a href = 'https://github.com/BgeeDB/bgee_pipeline/tree/develop/pipeline/dblite_creation' "
                 + "class='external_link' title ='link to Bgee lite documentation' target = '_blank'>"
-                + "Bgee lite</a> Relational database (MySQL).</p>");
+                + "Bgee lite</a> relational database.</p>");
+        
+        this.writeln("<p>It is reachable by using your preferable programming language or "
+                + "SPARQL interface through the URL address below : </p>");
+        this.writeln("<p><center>http://biosoda.expasy.org:8080/rdf4j-server/repositories/bgeelight</center></p>");
+        
+        this.writeln("<p><a href = '" + SPARQL_QUERY_EXAMPLE + "'"
+                + "title = 'SPARQL example query' class='external_link' "
+                + "target = '_blank'>Example URL</a>retrieving all anatomic entities in human where "
+                + "the apoc1 gene is expressed.</p>");
+        
+        this.writeln("<h1>Web Interface to query the Bgee SPARQL endpoint</h1>");
+        
         this.writeln("<p>It is also possible to run Bgee SPARQL queries using the "
                 + "<a href = 'http://biosoda.expasy.org:8080/build_biosodafrontend/' title = 'link to Bio-Query'"
                 + " class='external_link' target = '_blank'>Bio-Query Federated template</a> search created for "

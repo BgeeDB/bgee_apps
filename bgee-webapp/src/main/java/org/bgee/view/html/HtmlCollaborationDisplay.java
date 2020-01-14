2053ec006842f3b824aeafbfba7c196732cc562d
Sébastien Moretti <smoretti@unil.ch>
2020-01-14 17:00:46 +0100

    Fix unclosed P html tags

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlCollaborationDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlCollaborationDisplay.java
@@ -25,10 +25,10 @@ public class HtmlCollaborationDisplay extends HtmlParentDisplay implements Colla
     /**
      * Default constructor.
      *
-     * @param response          A {@code HttpServletResponse} that will be used to display the 
+     * @param response          A {@code HttpServletResponse} that will be used to display the
      *                          page to the client.
-     * @param requestParameters A {@code RequestParameters} handling the parameters of the 
-     *                          current request, to determine the requested displayType, 
+     * @param requestParameters A {@code RequestParameters} handling the parameters of the
+     *                          current request, to determine the requested displayType,
      *                          and for display purposes.
      * @param prop              A {@code BgeeProperties} instance that contains the properties
      *                          to use.
@@ -64,7 +64,7 @@ public class HtmlCollaborationDisplay extends HtmlParentDisplay implements Colla
                 "joint queries across resources in ways that cannot be anticipated and " +
                 "therefore goes far and above the query functionality of specialized knowledge bases. " +
                 "It is supported by <a href='http://www.nfp75.ch/en' target='_blank' rel='noopener' " +
-                "title='NFP75 Big Data website'>NFP75 'Big Data'</a>.<p>");
+                "title='NFP75 Big Data website'>NFP75 'Big Data'</a>.</p>");
 
         this.writeln("<p>For this project, we created a sub-database of Bgee called '" + BGEE_LITE_NAME +
                 "'. The MySQL dump is available on <a target='_blank' rel='noopener' title='Download the dump' href='" +
@@ -72,47 +72,47 @@ public class HtmlCollaborationDisplay extends HtmlParentDisplay implements Colla
                 "You can see the description of '" + BGEE_LITE_NAME + "' in <a title='" + BGEE_LITE_NAME +
                 " description' href='" + DEVELOP_BGEE_PIPELINE_GITHUB_URL + "/pipeline/dblite_creation" +
                 "#information-about-the-bgee-lite-database' target='_blank' rel='noopener'>" +
-                "Bgee pipeline documentation</a><p>");
+                "Bgee pipeline documentation</a></p>");
 
         this.writeln("<p>BioSODA uses the '" + BGEE_LITE_NAME + "' relational database to expose a " +
                 "Bgee SPARQL endpoint reachable through the following URL: " +
                 "<span class='copyable-url'>http://biosoda.expasy.org:8080/rdf4j-server/repositories/bgeelight</span>. " +
                 "In the context of this project, the <a href='http://biosoda.expasy.org' target='_blank' rel='noopener'>" +
                 "BioQuery web application</a> provides a user-friendly interface to query '" +
-                BGEE_LITE_NAME + "' database based on the SPARQL query language.<p>");
+                BGEE_LITE_NAME + "' database based on the SPARQL query language.</p>");
 
         this.writeln("<p>The SPARQL endpoint and '" + BGEE_LITE_NAME + "' are available and free " +
-                "to use for other projects or applications.<p>");
+                "to use for other projects or applications.</p>");
 
         this.writeln("<h2>OMA</h2>");
 
         this.writeln("<p>The <a href='https://omabrowser.org/oma/home/' target='_blank' rel='noopener'>OMA " +
                 "(Orthologous MAtrix) project</a> is a method and database " +
-                "for the inference of orthologs among complete genomes.<p>");
+                "for the inference of orthologs among complete genomes.</p>");
 
         this.writeln("<p>As part of a scientific collaboration with OMA, we generated files " +
                 "containing gene expression in homologous anatomical entities. " +
                 "These files are available on <a href='" +
                 this.prop.getFTPRootDirectory() +"collaboration/branch_length_expression_divergence/'>" +
-                "Bgee FTP</a>.<p>");
+                "Bgee FTP</a>.</p>");
 
         this.writeln("<p>The homologous expression files are also available and free " +
-                "to use for other projects or applications.<p>");
+                "to use for other projects or applications.</p>");
 
         this.writeln("<h2>OncoMX</h2>");
 
         this.writeln("<p><a href='https://www.oncomx.org/' target='_blank' rel='noopener'>OncoMX</a> is " +
                 "a knowledgebase of unified cancer genomics data from integrated mutation, " +
                 "expression, literature, and biomarker databases, accessible through web portal. " +
-                "It is supported by <a href='https://www.cancer.gov/' target='_blank' rel='noopener'>NIH NCI</a>.<p>");
-        
+                "It is supported by <a href='https://www.cancer.gov/' target='_blank' rel='noopener'>NIH NCI</a>.</p>");
+
         this.writeln("<p>For this project, we generated files containing a subset of calls of " +
                 "presence/absence of expression with expression level categories specific to OncoMX. " +
                 "Files are available on <a href='" + this.prop.getFTPRootDirectory() +
                 "/collaboration/oncoMX/'>Bgee FTP</a>. You can see the description of these files in " +
                 "<a href='" + DEVELOP_BGEE_PIPELINE_GITHUB_URL + "/pipeline/collaboration/oncoMX" +
                 "#information-about-the-files-generated-for-oncomx' target='_blank' rel='noopener'>" +
-                "Bgee pipeline documentation</a><p>");
+                "Bgee pipeline documentation</a></p>");
 
         this.writeln("</div>"); // close CENTERED_ELEMENT_CLASS
 
@@ -127,7 +127,7 @@ public class HtmlCollaborationDisplay extends HtmlParentDisplay implements Colla
     protected void includeCss() {
         log.entry();
         super.includeCss();
-        //If you ever add new files, you need to edit bgee-webapp/pom.xml 
+        //If you ever add new files, you need to edit bgee-webapp/pom.xml
         //to correctly merge/minify them.
         log.exit();
     }

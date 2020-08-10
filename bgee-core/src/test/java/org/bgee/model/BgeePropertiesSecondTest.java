3264e98537e708af0c9a8c06660c5911fe38c651
Julien <julien.wollbrett@gmail.com>
2020-08-10 15:52:49 +0200

    update tests

--- bgee-core/src/test/java/org/bgee/model/BgeePropertiesSecondTest.java
+++ bgee-core/src/test/java/org/bgee/model/BgeePropertiesSecondTest.java
@@ -42,6 +42,8 @@ public class BgeePropertiesSecondTest extends BgeePropertiesParentTest {
                 bgeeProp.getSearchServerURL());
         assertEquals("Wrong property value retrieved", "searchport",
                 bgeeProp.getSearchServerPort());
+        assertEquals("Wrong property value retrieved","3.10",
+                bgeeProp.getBioconductorReleaseNumber());
         assertEquals("Wrong property value retrieved","/filerexec",
                 bgeeProp.getTopAnatRScriptExecutable());
         assertEquals("Wrong property value retrieved","/filecallerwd",

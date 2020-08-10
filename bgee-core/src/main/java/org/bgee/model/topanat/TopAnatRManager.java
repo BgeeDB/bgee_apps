937d9f13177150d4da67bd63db4eeff0c1024459
Julien <julien.wollbrett@gmail.com>
2020-08-10 15:46:39 +0200

    update how to install R packages for topAnat

--- bgee-core/src/main/java/org/bgee/model/topanat/TopAnatRManager.java
+++ bgee-core/src/main/java/org/bgee/model/topanat/TopAnatRManager.java
@@ -79,6 +79,8 @@ public class TopAnatRManager {
                 anatEntitiesRelationshipsFileName, geneToAnatEntitiesFileName, foregroundIds);
 
         caller.setRscriptExecutable(this.props.getTopAnatRScriptExecutable());
+        
+        String bioconductorRelease = this.props.getBioconductorReleaseNumber();
 
         code.clear();
         code.addRCode("# Check that you have installed the required packages");
@@ -86,23 +88,23 @@ public class TopAnatRManager {
         code.addRCode("  install.packages('BiocManager')");
         code.addRCode("}");
         code.addRCode("if(!suppressWarnings(require(Rgraphviz))){");
-        code.addRCode("  BiocManager::install('Rgraphviz')");
+        code.addRCode("  BiocManager::install('Rgraphviz', version='" + bioconductorRelease + ")");
         code.addRCode("}");
 
         code.addRCode("if(!suppressWarnings(require(Runiversal))){");
-        code.addRCode("  BiocManager::install('Runiversal')");
+        code.addRCode("  BiocManager::install('Runiversal', version='" + bioconductorRelease + ")");
         code.addRCode("}");
 
         // if there is no decorrelation, do not use the topGO package
         if(this.params.getDecorrelationType() != DecorrelationType.NONE){
             code.addRCode("packageExistTopGO<-require()");
             code.addRCode("if(!suppressWarnings(require(topGO))){");
-            code.addRCode("  BiocManager::install('topGO')");
+            code.addRCode("  BiocManager::install('topGO', version='" + bioconductorRelease + ")");
             code.addRCode("}");
         }
 
         code.addRCode("if(!suppressWarnings(require(rJava))){");
-        code.addRCode("  BiocManager::install('rJava')");
+        code.addRCode("  BiocManager::install('rJava', version='" + bioconductorRelease + ")");
         code.addRCode("}");
 
         if(this.params.getDecorrelationType() != DecorrelationType.NONE){

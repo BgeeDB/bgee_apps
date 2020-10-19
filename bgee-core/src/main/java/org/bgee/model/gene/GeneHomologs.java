21fbf6a33d6b2c68f6033f605bcbba80db1bfeb9
Julien <julien.wollbrett@gmail.com>
2020-10-19 16:53:16 +0200

    update GeneHomologs class

--- bgee-core/src/main/java/org/bgee/model/gene/GeneHomologs.java
+++ bgee-core/src/main/java/org/bgee/model/gene/GeneHomologs.java
@@ -1,43 +1,63 @@
 package org.bgee.model.gene;
 
-import org.apache.commons.lang.StringUtils;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bgee.model.species.Taxon;
 
 /**
- * A class describing gene homologs. It can be an ortholg or a paralog
+ * A class describing a gene and its homology relations (orthologs and/or paralogs). 
+ * Homology are stored as {@code LinkedHashMap} where the key correspond to {@code Taxon} and
+ * Keys correspond to {@code Set} of {@code Gene} in order to keep proper Taxon order (e.g more
+ * recent to oldest).
+ * 
  * 
  * @author Julien Wollbrett
- * @version Bgee 15 Sep. 2020
+ * @version Bgee 15 Oct. 2020
  * @since Bgee 15 Sep. 2020
  * @see Gene 
  */
-public class GeneHomolog {
+public class GeneHomologs {
     
-    private final static Logger log = LogManager.getLogger(GeneHomolog.class.getName());
+    private final static Logger log = LogManager.getLogger(GeneHomologs.class.getName());
     
-    private Gene gene;
-    private Integer taxonId;
+    private final Gene gene;
+    private final LinkedHashMap<Taxon, Set<Gene>> orthologsByTaxon;
+    private final LinkedHashMap<Taxon, Set<Gene>> paralogsByTaxon;
     
-    public GeneHomolog(Gene gene, Integer taxonId) {
+    public GeneHomologs(Gene gene, LinkedHashMap<Taxon, Set<Gene>> orthologsByTaxon,
+            LinkedHashMap<Taxon, Set<Gene>> paralogsByTaxon) {
         if (gene == null) {
             throw log.throwing(new IllegalArgumentException("a gene can not be null"));
         }
+        if((orthologsByTaxon == null || orthologsByTaxon.isEmpty()) &&
+                (paralogsByTaxon == null || paralogsByTaxon.isEmpty())) {
+            throw log.throwing(new IllegalArgumentException("orthologs or paralogs have "
+                    + "to be provided. They can not both be null or empty"));
+        }
         this.gene = gene;
-        this.taxonId = taxonId;
+        this.orthologsByTaxon = orthologsByTaxon;
+        this.paralogsByTaxon = paralogsByTaxon;
     }
     
     public Gene getGene() {
         return gene;
     }
 
-    public Integer getTaxonId() {
-        return taxonId;
+    public LinkedHashMap<Taxon, Set<Gene>> getOrthologsByTaxon() {
+        return orthologsByTaxon;
+    }
+
+    public LinkedHashMap<Taxon, Set<Gene>> getParalogsByTaxon() {
+        return paralogsByTaxon;
     }
 
     @Override
     public String toString() {
-        return "GeneHomolog [gene=" + gene + ", taxonId=" + taxonId + "]";
+        return "GeneHomolog [gene=" + gene + ", orthologsByTaxon=" + orthologsByTaxon + ", paralogsByTaxon="
+                + paralogsByTaxon + "]";
     }
 
     @Override
@@ -45,7 +65,8 @@ public class GeneHomolog {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((gene == null) ? 0 : gene.hashCode());
-        result = prime * result + ((taxonId == null) ? 0 : taxonId.hashCode());
+        result = prime * result + ((orthologsByTaxon == null) ? 0 : orthologsByTaxon.hashCode());
+        result = prime * result + ((paralogsByTaxon == null) ? 0 : paralogsByTaxon.hashCode());
         return result;
     }
 
@@ -57,19 +78,26 @@ public class GeneHomolog {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        GeneHomolog other = (GeneHomolog) obj;
+        GeneHomologs other = (GeneHomologs) obj;
         if (gene == null) {
             if (other.gene != null)
                 return false;
         } else if (!gene.equals(other.gene))
             return false;
-        if (taxonId == null) {
-            if (other.taxonId != null)
+        if (orthologsByTaxon == null) {
+            if (other.orthologsByTaxon != null)
                 return false;
-        } else if (!taxonId.equals(other.taxonId))
+        } else if (!orthologsByTaxon.equals(other.orthologsByTaxon))
+            return false;
+        if (paralogsByTaxon == null) {
+            if (other.paralogsByTaxon != null)
+                return false;
+        } else if (!paralogsByTaxon.equals(other.paralogsByTaxon))
             return false;
         return true;
     }
+
+
     
     
 

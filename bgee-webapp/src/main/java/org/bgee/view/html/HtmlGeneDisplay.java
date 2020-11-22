5c5e681548307467a04f70f51ff3c15d9fae1c70
Julien <julien.wollbrett@gmail.com>
2020-11-22 17:30:09 +0100

    update sentence source of homology

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlGeneDisplay.java
@@ -492,7 +492,7 @@ public class HtmlGeneDisplay extends HtmlParentDisplay implements GeneDisplay {
             this.writeln("</div>"); // end class
             
             this.writeln("<div id='homology_source' class='col-xs-offset-1 col-sm-offset-2 col-sm-9 col-md-offset-0 col-md-10'>");
-            this.writeln("<p>Orthology come from OMA : <a  target='_blank' rel='noopener' "
+            this.writeln("<p>Orthology information come from OMA : <a  target='_blank' rel='noopener' "
                     + "href='https://omabrowser.org/oma/vps/" + gene.getEnsemblGeneId() 
                     + "'>" + gene.getEnsemblGeneId() + "</a>.</p>");
             this.writeln("</div>");
@@ -514,7 +514,7 @@ public class HtmlGeneDisplay extends HtmlParentDisplay implements GeneDisplay {
             this.writeln("</div>"); // end class
             
             this.writeln("<div id='homology_source' class='col-xs-offset-1 col-sm-offset-2 col-sm-9 col-md-offset-0 col-md-10'>");
-            this.writeln("<p>Paralogy come from OMA : <a  target='_blank' rel='noopener' "
+            this.writeln("<p>Paralogy information come from OMA : <a  target='_blank' rel='noopener' "
                     + "href='https://omabrowser.org/oma/pps/" + gene.getEnsemblGeneId() 
                     + "'>" + gene.getEnsemblGeneId() + "</a>.</p>");
             this.writeln("</div>");

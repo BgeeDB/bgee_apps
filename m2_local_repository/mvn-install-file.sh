a33ccb49deceffd1644d6b4f2491fabb98c0101b
fbastian <frederic.bastian@gmail.com>
2020-12-24 00:24:13 +0100

    Allow versioning of owltools in local m2 repository

--- m2_local_repository/mvn-install-file.sh
+++ m2_local_repository/mvn-install-file.sh
@@ -2,7 +2,8 @@
 
 #proper file: OWLTools-Runner/bin/owltools-runner-all.jar
 rm -rf ~/.m2/repository/org/bbop/
-mvn install:install-file -Dfile=source_libraries/owltools-runner-all.jar -DgroupId=org.bbop -DartifactId=OWLTools-all -Dversion=0.3.0-SNAPSHOT-owlapi-5.1.17 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=repository/
+mvn install:install-file -Dfile=source_libraries/owltools-runner-all-owlapi-5.1.17.jar -DgroupId=org.bbop -DartifactId=OWLTools-all -Dversion=0.3.0-SNAPSHOT-owlapi-5.1.17 -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=repository/
+mvn install:install-file -Dfile=source_libraries/owltools-runner-all.jar -DgroupId=org.bbop -DartifactId=OWLTools-all -Dversion=0.3.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=repository/
 #Note that the file ncbi2owl.jar in the owltools project is located in OWLTools-NCBI/bin/
 mvn install:install-file -Dfile=source_libraries/ncbi2owl.jar -DgroupId=org.bbop -DartifactId=OWLTools-ncbi -Dversion=0.3.0-SNAPSHOT -Dpackaging=jar -DgeneratePom=true -DlocalRepositoryPath=repository/
 

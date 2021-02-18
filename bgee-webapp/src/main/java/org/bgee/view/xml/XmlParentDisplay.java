c8e4c47e3d8d5489941ba561d2921d44099eb9ff
fbastian <frederic.bastian@gmail.com>
2021-02-18 15:48:47 +0100

    Update location of StringEscapeUtils that is now in package commons-text

--- bgee-webapp/src/main/java/org/bgee/view/xml/XmlParentDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/xml/XmlParentDisplay.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.bgee.controller.BgeeProperties;

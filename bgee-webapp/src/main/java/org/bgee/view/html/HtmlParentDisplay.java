275d8024abf574f2e33e6ad69354b412e8347f80
fbastian <frederic.bastian@gmail.com>
2021-02-25 18:43:41 +0100

    Update Bgee description

--- bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
+++ bgee-webapp/src/main/java/org/bgee/view/html/HtmlParentDisplay.java
@@ -247,14 +247,14 @@ public class HtmlParentDisplay extends ConcreteDisplayParent {
         this.writeln("<meta charset='UTF-8'>");
         this.writeln("<meta name='viewport' content='width=device-width, initial-scale=1.0'>");
         this.writeln("<title>"+title+"</title>");
-        this.writeln("<meta name='description' content='Bgee allows to automatically"
-                + " compare gene expression patterns between species, by referencing"
-                + " expression data on anatomical ontologies, and designing homology"
-                + " relationships between them.'/>");
-        this.writeln("<meta name='keywords' content='bgee, gene expression, "
-                + "evolution, ontology, anatomy, development, evo-devo database, "
-                + "anatomical ontology, developmental ontology, gene expression "
-                + "evolution'/>");
+        this.writeln("<meta name='description' content='Bgee is a database "
+            + "for retrieval and comparison of gene expression patterns "
+            + "across multiple animal species. It provides an intuitive answer to the question "
+            + "-where is a gene expressed?- and supports research in cancer and agriculture "
+            + "as well as evolutionary biology.'/>");
+        this.writeln("<meta name='keywords' content='bgee, gene expression, evolution, "
+                + "ontology, anatomy, development, evo-devo database, anatomical ontology, "
+                + "developmental ontology, gene expression evolution'/>");
         this.writeln("<meta name='dcterms.rights' content='Bgee copyright 2007/"
                 + ZonedDateTime.now(ZoneId.of("Europe/Zurich")).getYear()
                 + " UNIL' />");

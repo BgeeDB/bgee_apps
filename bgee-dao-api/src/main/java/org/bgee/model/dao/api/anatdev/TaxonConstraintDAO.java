43c058ba87f9dc5d9b13677c37a6010e9ae41cd9
fbastian <frederic.bastian@gmail.com>
2021-05-10 18:46:06 +0200

    Allow to filter retrieval of anat. entity taxon constraints using anat. entity IDs

--- bgee-dao-api/src/main/java/org/bgee/model/dao/api/anatdev/TaxonConstraintDAO.java
+++ bgee-dao-api/src/main/java/org/bgee/model/dao/api/anatdev/TaxonConstraintDAO.java
@@ -54,6 +54,28 @@ public interface TaxonConstraintDAO {
     public TaxonConstraintTOResultSet<String> getAnatEntityTaxonConstraints(
             Collection<Integer> speciesIds, Collection<TaxonConstraintDAO.Attribute> attributes)
             throws DAOException;
+    /**
+     * Retrieve anatomical entity taxon constrains from data source.
+     * The constrains can be filtered by species IDs and anat. entity IDs.
+     * <p>
+     * The taxon constrains are retrieved and returned as a {@code TaxonConstraintTOResultSet}.
+     * It is the responsibility of the caller to close this {@code DAOResultSet}
+     * once results are retrieved.
+     *
+     * @param speciesIds    A {@code Collection} of {@code Integer}s that are the IDs of species
+     *                      to retrieve taxon constrains for.
+     * @param anatEntityIds A {@code Collection} of {@code String}s that are the IDs of anat. entities
+     *                      to retrieve taxon constrains for.
+     * @param attributes    A {@code Collection} of {@code TaxonConstraintDAO.Attribute}s defining
+     *                      the attributes to populate in the returned {@code TaxonConstraintTO}s.
+     *                      If {@code null} or empty, all attributes are populated.
+     * @return              A {@code TaxonConstraintTOResultSet} allowing to retrieve
+     *                      anatomical entity taxon constrains from data source.
+     * @throws DAOException If an error occurred when accessing the data source.
+     */
+    public TaxonConstraintTOResultSet<String> getAnatEntityTaxonConstraints(
+            Collection<Integer> speciesIds, Collection<String> anatEntityIds,
+            Collection<TaxonConstraintDAO.Attribute> attributes) throws DAOException;
     
     /**
      * Retrieve anatomical entity relation taxon constrains from data source.

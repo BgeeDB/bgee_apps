75457e9998f1e64f0f950c89f5f0e4b1a7f0d21c
Julien <julien.wollbrett@gmail.com>
2022-11-11 15:04:56 +0100

    misc modifs in RawDataPostFilter

--- bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/RawDataPostFilter.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/RawDataPostFilter.java
@@ -1,10 +1,16 @@
 package org.bgee.model.expressiondata.rawdata;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.bgee.model.anatdev.AnatEntity;
 import org.bgee.model.anatdev.DevStage;
+import org.bgee.model.expressiondata.baseelements.DataType;
 import org.bgee.model.expressiondata.rawdata.RawDataCondition.RawDataSex;
 /**
  * A class containing values of all condition parameters used to create filters
@@ -16,54 +22,76 @@ import org.bgee.model.expressiondata.rawdata.RawDataCondition.RawDataSex;
  */
 public class RawDataPostFilter {
 
-    private final Collection<AnatEntity> anatEntities;
-    private final Collection<DevStage> devStages;
-    private final Collection<AnatEntity> cellTypes;
-    private final Collection<RawDataSex> sexes;
-    private final Collection<String> strains;
+    private final Set<AnatEntity> anatEntities;
+    private final Set<DevStage> devStages;
+    private final Set<AnatEntity> cellTypes;
+    private final Set<RawDataSex> sexes;
+    private final Set<String> strains;
+    private final DataType requestedDatatype;
 
     /**
      * 
-     * @param anatEntities  A {@code Collection} of {@code AnatEntity} specifying the anatomical
-     *                      entities to use in the filtering
-     * @param devStages     A {@code Collection} of {@code DevStage} specifying the developmental
-     *                      stages to use in the filtering
-     * @param cellTypes     A {@code Collection} of {@code AnatEntity} specifying the cell types
-     *                      to use in the filtering
-     * @param sexes         A {@code Collection} of {@code RawDataSex} specifying the sexes
-     *                      to use in the filtering
-     * @param strains       A {@code Collection} of {@code String} specifying the strains to use
-     *                      in the filtering
+     * @param anatEntities      A {@code Collection} of {@code AnatEntity} specifying the anatomical
+     *                          entities to use in the filtering
+     * @param devStages         A {@code Collection} of {@code DevStage} specifying the developmental
+     *                          stages to use in the filtering
+     * @param cellTypes         A {@code Collection} of {@code AnatEntity} specifying the cell types
+     *                          to use in the filtering
+     * @param sexes             A {@code Collection} of {@code RawDataSex} specifying the sexes
+     *                          to use in the filtering
+     * @param strains           A {@code Collection} of {@code String} specifying the strains to use
+     *                          in the filtering
+     * @param requestedDatatype A {@code DataType} corresponding to the data type for which post
+     *                          fitlers have been retrieved.
      */
     public RawDataPostFilter(Collection<AnatEntity> anatEntities, Collection<DevStage> devStages,
             Collection<AnatEntity> cellTypes, Collection<RawDataSex> sexes,
-            Collection<String> strains) {
+            Collection<String> strains, DataType requestedDatatype) {
         super();
-        this.anatEntities = anatEntities;
-        this.devStages = devStages;
-        this.cellTypes = cellTypes;
-        this.sexes = sexes;
-        this.strains = strains;
+        this.anatEntities = anatEntities == null ? null :
+            Collections.unmodifiableSet(new LinkedHashSet<>(anatEntities.stream()
+                    .sorted(Comparator.comparing(ae -> ae.getName()))
+                    .collect(Collectors.toCollection(LinkedHashSet::new))));
+        this.devStages = devStages == null ? null :
+            Collections.unmodifiableSet(new LinkedHashSet<>(devStages.stream()
+                    .sorted(Comparator.comparing(ds -> ds.getName()))
+                    .collect(Collectors.toCollection(LinkedHashSet::new))));
+        this.cellTypes = cellTypes == null ? null :
+            Collections.unmodifiableSet(new LinkedHashSet<>(cellTypes.stream()
+                    .sorted(Comparator.comparing(ct -> ct.getName()))
+                    .collect(Collectors.toCollection(LinkedHashSet::new))));
+        this.sexes = sexes == null ? null :
+            Collections.unmodifiableSet(new LinkedHashSet<>(sexes.stream()
+                    .sorted(Comparator.comparing(s -> s.getStringRepresentation()))
+                    .collect(Collectors.toCollection(LinkedHashSet::new))));
+        this.strains = strains == null ? null :
+            Collections.unmodifiableSet(new LinkedHashSet<>(strains.stream().sorted()
+                    .collect(Collectors.toSet())));
+        this.requestedDatatype = requestedDatatype;
     }
 
-    public Collection<AnatEntity> getAnatEntities() {
+    public Set<AnatEntity> getAnatEntities() {
         return anatEntities;
     }
 
-    public Collection<DevStage> getDevStages() {
+    public Set<DevStage> getDevStages() {
         return devStages;
     }
 
-    public Collection<AnatEntity> getCellTypes() {
+    public Set<AnatEntity> getCellTypes() {
         return cellTypes;
     }
 
-    public Collection<RawDataSex> getSexes() {
+    public Set<RawDataSex> getSexes() {
         return sexes;
     }
 
-    public Collection<String> getStrains() {
+    public Set<String> getStrains() {
         return strains;
+    }
+
+    public DataType getRequestedDatatype() {
+        return requestedDatatype;
     }
 
     @Override

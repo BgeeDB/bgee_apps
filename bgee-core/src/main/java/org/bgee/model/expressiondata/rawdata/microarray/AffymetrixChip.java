adb655374379410ca106868b886a26b993462aa8
fbastian <frederic.bastian@gmail.com>
2022-11-25 01:03:49 +0100

    Define the primary key of AffymetrixChip as ID and experiment

--- bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/microarray/AffymetrixChip.java
+++ bgee-core/src/main/java/org/bgee/model/expressiondata/rawdata/microarray/AffymetrixChip.java
@@ -9,6 +9,8 @@ import org.bgee.model.expressiondata.rawdata.baseelements.AssayPartOfExp;
 import org.bgee.model.expressiondata.rawdata.baseelements.RawDataAnnotated;
 import org.bgee.model.expressiondata.rawdata.baseelements.RawDataAnnotation;
 
+//AffymetrixChip IDs are not unique, they are unique inside a given experiment.
+//This is why this class does not extend Entity.
 public class AffymetrixChip implements AssayPartOfExp<AffymetrixExperiment>, RawDataAnnotated {
     private final static Logger log = LogManager.getLogger(AffymetrixChip.class.getName());
 
@@ -62,12 +64,11 @@ public class AffymetrixChip implements AssayPartOfExp<AffymetrixExperiment>, Raw
     }
 
     //AffymetrixChip IDs are not unique, they are unique inside a given experiment.
-    //This is why we reimplement hashCode/equals rather than using the 'Entity' implementation.
+    //We use the ID and the experiment as primary key
     @Override
     public int hashCode() {
-        return Objects.hash(annotation, chipType, experiment, id, pipelineSummary);
+        return Objects.hash(experiment, id);
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -77,15 +78,19 @@ public class AffymetrixChip implements AssayPartOfExp<AffymetrixExperiment>, Raw
         if (getClass() != obj.getClass())
             return false;
         AffymetrixChip other = (AffymetrixChip) obj;
-        return Objects.equals(annotation, other.annotation) && Objects.equals(chipType, other.chipType)
-                && Objects.equals(experiment, other.experiment) && Objects.equals(id, other.id)
-                && Objects.equals(pipelineSummary, other.pipelineSummary);
+        return Objects.equals(experiment, other.experiment) && Objects.equals(id, other.id);
     }
 
     @Override
     public String toString() {
-        return "AffymetrixChip [id=" + id + ", experiment=" + experiment + ", annotation=" + annotation + ", chipType="
-                + chipType + ", pipelineSummary=" + pipelineSummary + "]";
+        StringBuilder builder = new StringBuilder();
+        builder.append("AffymetrixChip [")
+               .append("id=").append(id)
+               .append(", experiment=").append(experiment)
+               .append(", annotation=").append(annotation)
+               .append(", chipType=").append(chipType)
+               .append(", pipelineSummary=").append(pipelineSummary)
+               .append("]");
+        return builder.toString();
     }
-
 }

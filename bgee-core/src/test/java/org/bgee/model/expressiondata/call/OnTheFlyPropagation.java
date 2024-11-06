5669e3e1a3542816a94391db58e4d94cc7fa60c0
Julien Wollbrett <julien.wollbrett@unil.ch>
2024-11-06 15:23:01 +0100

    create map of raw condition to condition

--- bgee-core/src/test/java/org/bgee/model/expressiondata/call/OnTheFlyPropagation.java
+++ bgee-core/src/test/java/org/bgee/model/expressiondata/call/OnTheFlyPropagation.java
@@ -1,8 +1,18 @@
 package org.bgee.model.expressiondata.call;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.bgee.model.expressiondata.rawdata.baseelements.RawDataCondition;
 import org.bgee.model.gene.Gene;
 
 public class OnTheFlyPropagation {
+
+    private final static Logger log = LogManager.getLogger(OnTheFlyPropagation.class.getName());
 
     public void launchTest() {
 
@@ -18,5 +28,14 @@ public class OnTheFlyPropagation {
             
             
         }
+    }
+
+    public Map<RawDataCondition, Condition>
+        loadRawCondToCondMap(Set<RawDataCondition> rawDataConditions,
+                Collection<CallService.Attribute> condParameters) {
+        log.traceEntry("{}, {}", rawDataConditions, condParameters);
+        return log.traceExit(rawDataConditions.stream()
+                .collect(Collectors.toMap(c -> c, c -> c.toCondition(condParameters))));
+
     }
 }
